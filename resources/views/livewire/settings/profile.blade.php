25ab49a1ab4fe5d813f430cf2670760e0c18310a
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-28 07:48:44 +0800

    af

--- resources/views/livewire/settings/profile.blade.php
+++ resources/views/livewire/settings/profile.blade.php
@@ -3,7 +3,7 @@
 
     <x-settings.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
         <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
                 <flux:input wire:model.blur="name" :label="__('Name')" type="text" autofocus autocomplete="name" />
                 <flux:input wire:model.blur="username" :label="__('username')" type="text" autofocus
                     autocomplete="username" />
