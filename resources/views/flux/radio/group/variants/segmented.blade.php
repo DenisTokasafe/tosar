ff7aa2431c2ed8c7ea0ec901b5270b8989e61d88
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-27 10:05:56 +0800

    sas

--- resources/views/flux/radio/group/variants/segmented.blade.php
+++ resources/views/flux/radio/group/variants/segmented.blade.php
@@ -15,7 +15,7 @@ if (! isset($name)) {
 
 $classes = Flux::classes()
     ->add('block flex p-1')
-    ->add('rounded-lg bg-zinc-800/5 dark:bg-black')
+    ->add('rounded-lg bg-black')
     ->add($size === 'sm' ? 'h-8 py-[3px] px-[3px]' : 'h-10 p-1')
     ->add($size === 'sm' ? '-my-px h-[calc(2rem+2px)]' : '')
     ;
