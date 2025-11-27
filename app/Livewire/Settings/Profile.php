bfdc81d16f5ea97e2c607a30082a82b8177d67bf
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-27 10:44:46 +0800

    sas

--- app/Livewire/Settings/Profile.php
+++ app/Livewire/Settings/Profile.php
@@ -3,18 +3,31 @@
 namespace App\Livewire\Settings;
 
 use App\Models\User;
+use Livewire\Component;
+use App\Models\Contractor;
+use App\Models\Department;
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Validation\Rule;
-use Livewire\Component;
 
 class Profile extends Component
 {
     public string $name = '';
     public string $username = '';
-
+    #[Validate('required_without:contractor_id')]
+    public $department_id;
+    #[Validate('required_without:department_id')]
+    public $contractor_id;
     public string $email = '';
-
+    public $deptCont = 'department';
+    public $search = '';
+    public $dep_cont = '';
+    public $departments = [];
+    public $contractors = [];
+    public $showDropdown = false;
+    public $searchContractor = '';
+    public $showContractorDropdown = false;
     /**
      * Mount the component.
      */
@@ -23,6 +36,51 @@ class Profile extends Component
         $this->username = Auth::user()->username;
         $this->name = Auth::user()->name;
         $this->email = Auth::user()->email;
+    }
+    public function updatedSearch()
+    {
+        if (strlen($this->search) > 1) {
+            $this->departments = Department::where('department_name', 'like', '%' . $this->search . '%')
+                ->orderBy('department_name')
+                ->limit(10)
+                ->get();
+            $this->showDropdown = true;
+        } else {
+            $this->departments = [];
+            $this->showDropdown = false;
+        }
+    }
+    public function selectDepartment($id, $name)
+    {
+        $this->reset('searchContractor', 'contractor_id');
+        $this->department_id = $id;
+        $this->search = $name;
+        $this->dep_cont = $name;
+        $this->showDropdown = false;
+        $this->validateOnly('department_id');
+    }
+    public function updatedSearchContractor()
+    {
+        if (strlen($this->searchContractor) > 1) {
+            $this->contractors = Contractor::query()
+                ->where('contractor_name', 'like', '%' . $this->searchContractor . '%')
+                ->orderBy('contractor_name')
+                ->limit(10)
+                ->get();
+            $this->showContractorDropdown = true;
+        } else {
+            $this->contractors = [];
+            $this->showContractorDropdown = true;
+        }
+    }
+    public function selectContractor($id, $name)
+    {
+        $this->reset('search', 'department_id');
+        $this->contractor_id = $id;
+        $this->searchContractor = $name;
+        $this->dep_cont = $name;
+        $this->showContractorDropdown = false;
+        $this->validateOnly('contractor_id');
     }
 
     /**
--- resources/views/livewire/settings/profile.blade.php
+++ resources/views/livewire/settings/profile.blade.php
@@ -4,17 +4,85 @@
     <x-settings.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
         <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
             <flux:input wire:model="name" :label="__('Name')" type="text" required autofocus autocomplete="name" />
-            <flux:input wire:model="username" :label="__('username')" type="text" required autofocus autocomplete="username" />
+            <flux:input wire:model="username" :label="__('username')" type="text" required autofocus
+                autocomplete="username" />
+            <fieldset>
+                <input id="department" value="department" wire:model="deptCont"
+                    class="peer/department radio radio-xs radio-accent" type="radio" name="deptCont" checked />
+                <x-form.label for="department" class="peer-checked/department:text-accent text-[10px]"
+                    label="PT. MSM & PT. TTN" required />
+                <input id="contractor" value="contractor" wire:model="deptCont"
+                    class="peer/contractor radio radio-xs radio-primary" type="radio" name="deptCont" />
+                <x-form.label for="contractor" class="peer-checked/contractor:text-primary" label="Kontraktor"
+                    required />
 
+                <div class="hidden peer-checked/department:block mt-0.5">
+                    {{-- Department --}}
+                    <div class="relative mb-1">
+                        <!-- Input Search -->
+                        <input name="search" type="text" wire:model.live.debounce.300ms="search"
+                            placeholder="Cari departemen..."
+                            class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs {{ $errors->has('department_id') ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '' }}" />
+                        <!-- Dropdown hasil search -->
+                        @if ($showDropdown && count($departments) > 0)
+                            <ul
+                                class="absolute z-10 bg-base-100 border rounded-md w-full mt-1 max-h-60 overflow-auto shadow">
+                                <!-- Spinner ketika klik salah satu -->
+                                <div wire:loading wire:target="selectDepartment" class="p-2 text-center">
+                                    <span class="loading loading-spinner loading-sm text-secondary"></span>
+                                </div>
+                                @foreach ($departments as $dept)
+                                    <li wire:click="selectDepartment({{ $dept->id }}, '{{ $dept->department_name }}')"
+                                        class="px-3 py-2 cursor-pointer hover:bg-base-200">
+                                        {{ $dept->department_name }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    </div>
+                    @if ($deptCont === 'department')
+                        <x-label-error :messages="$errors->get('department_id')" />
+                    @endif
+                </div>
+                <div class="hidden peer-checked/contractor:block mt-0.5">
+                    {{-- Contractor --}}
+                    <div class="relative mb-1">
+                        <!-- Input Search -->comp
+                        <input name="searchContractor" type="text" wire:model.live.debounce.300ms="searchContractor"
+                            placeholder="Cari kontraktor..."
+                            class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs {{ $errors->has('contractor_id') ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '' }}" />
+                        <!-- Dropdown hasil search -->
+                        @if ($showContractorDropdown && count($contractors) > 0)
+                            <ul
+                                class="absolute z-10 bg-base-100 border rounded-md w-full mt-1 max-h-60 overflow-auto shadow">
+                                <!-- Spinner ketika klik -->
+                                <div wire:loading wire:target="selectContractor" class="p-2 text-center">
+                                    <span class="loading loading-spinner loading-sm text-secondary"></span>
+                                </div>
+                                @foreach ($contractors as $contractor)
+                                    <li wire:click="selectContractor({{ $contractor->id }}, '{{ $contractor->contractor_name }}')"
+                                        class="px-3 py-2 cursor-pointer hover:bg-base-200">
+                                        {{ $contractor->contractor_name }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    </div>
+                    @if ($deptCont === 'contractor')
+                        <x-label-error :messages="$errors->get('contractor_id')" />
+                    @endif
+                </div>
+            </fieldset>
             <div>
                 <flux:input wire:model="email" :label="__('Email')" type="email" required autocomplete="email" />
 
-                @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail &&! auth()->user()->hasVerifiedEmail())
+                @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !auth()->user()->hasVerifiedEmail())
                     <div>
                         <flux:text class="mt-4">
                             {{ __('Your email address is unverified.') }}
 
-                            <flux:link class="text-sm cursor-pointer" wire:click.prevent="resendVerificationNotification">
+                            <flux:link class="text-sm cursor-pointer"
+                                wire:click.prevent="resendVerificationNotification">
                                 {{ __('Click here to re-send the verification email.') }}
                             </flux:link>
                         </flux:text>
