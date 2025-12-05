06b1441295c7ce7d2d833cc4df3f30eb481fc31a
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-05 13:34:34 +0800

    asfd

--- resources/views/livewire/manhours/index.blade.php
+++ resources/views/livewire/manhours/index.blade.php
@@ -48,20 +48,18 @@
                         });
                     },
                     clearDate() {
-                        if (this.fp) this.fp.clear();
-                        $wire.set('range_date', null);
-                        // Panggil updatedRangeDate agar filter terhapus di Livewire
-                        $wire.call('updatedRangeDate', null);
+                        if (this.fp) this.fp.clear(); // 🔥 kosongkan input di flatpickr
+                        $wire.set('range_date', null); // 🔥 kosongkan properti Livewire
                     }
                 }" x-init="initFlatpickr();
                 Livewire.hook('message.processed', () => initFlatpickr());" x-ref="wrapper">
 
                     <input name="range_date" type="text" x-ref="tanggalInput2" wire:model.live="range_date"
-                        placeholder="Pilih Rentang Tanggal"
+                        placeholder="Pilih Tanggal"
                         class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs join-item"
                         readonly />
 
-                    <button @click="clearDate()" class="btn btn-xs btn-neutral join-item" type="button"
+                    <label @click="clearDate(); $wire.call('clearFilter')" class="btn btn-xs btn-neutral join-item"
                         title="Bersihkan Filter">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -71,7 +69,7 @@
                             <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
                             <path d="M8 16H3v5" />
                         </svg>
-                    </button>
+                    </label>
                 </div>
             </div>
         </div>
