72b8e3b0ea28b5a76d904e16b044424c31aab91b
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-05 09:06:31 +0800

    sas

--- resources/views/livewire/manhours/manhours-import.blade.php
+++ resources/views/livewire/manhours/manhours-import.blade.php
@@ -4,14 +4,6 @@
             <flux:button size="xs" wire:click='openModal' icon="import" variant="accent"></flux:button>
         </flux:tooltip>
     {{-- Notifikasi Sukses/Gagal (di luar modal agar terlihat setelah modal tertutup) --}}
-    @if ($message && $status == 'success')
-        <div class="toast toast-end mt-4">
-            <div class="alert alert-success">
-                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>{{ $message }}</span>
-            </div>
-        </div>
-    @endif
 
 
     {{-- 2. Modal Daisy UI --}}
