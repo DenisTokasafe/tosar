9e5e349b8765e3ca3d1d99bc3008bf7d2c7a8c25
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-05 09:07:16 +0800

    sdf

--- resources/views/livewire/manhours/manhours-import.blade.php
+++ resources/views/livewire/manhours/manhours-import.blade.php
@@ -13,15 +13,6 @@
         <div class="modal-box">
             <h3 class="font-bold text-lg">Import Data Manhours</h3>
             <p class="py-4">Unggah file Excel (.xlsx, .xls) atau CSV yang berisi data manhours.</p>
-
-            {{-- Pesan Error Validasi/Import --}}
-            @if ($message && $status == 'error')
-                <div class="alert alert-error my-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                    <span>{{ $message }}</span>
-                </div>
-            @endif
-
             <form wire:submit="import" enctype="multipart/form-data">
 
                 {{-- Input File --}}
