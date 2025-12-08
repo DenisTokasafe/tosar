57894b5f84095d6bed26be250788d33b726a6968
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-08 13:33:51 +0800

    sas

--- resources/views/livewire/manhours/grafik/index.blade.php
+++ resources/views/livewire/manhours/grafik/index.blade.php
@@ -7,6 +7,7 @@
             var dom = document.getElementById('hazardTrend');
             var myChart = echarts.init(dom);
             var option;
+            console.log(data);
 
             option = {
                 title: {
