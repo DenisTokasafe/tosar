7369d7c1aec01c333462da0f444c0b23c15c2b32
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-08 14:08:11 +0800

    asdfa

--- app/Livewire/Manhours/Grafik/Index.php
+++ app/Livewire/Manhours/Grafik/Index.php
@@ -15,6 +15,11 @@ class Index extends Component
 
     public function mount()
     {
+        // Default filter tanggal
+        if (!$this->start_date || !$this->end_date) {
+            $this->start_date = Carbon::now()->startOfYear()->format('Y/m/d');
+            $this->end_date = Carbon::now()->endOfYear()->format('Y/m/d');
+        }
         $this->loadData();
     }
     #[On('chartManhoursUpdate')]
