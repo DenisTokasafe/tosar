10b50c6e09dcccac0dc7257420a5d6fc1b7eb1f9
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-09 14:34:33 +0800

    asdfadsf

--- resources/views/livewire/hazard/hazard-report-panel.blade.php
+++ resources/views/livewire/hazard/hazard-report-panel.blade.php
@@ -2,8 +2,8 @@
     <x-toast />
     @include('partials.header-hazard')
     <div class="flex flex-col md:flex-row md:justify-between items-start md:items-center">
-        <div class="mb-4 md:mb-0 flex items-center gap-2 mt-0.5">
-            <div class="tooltip tooltip-right " data-tip="Tambah Hazard">
+        <div class="mb-4 md:mb-0 flex items-center gap-2">
+            <div class="tooltip tooltip-right  mt-0.5" data-tip="Tambah Hazard">
                 <a href="{{ route('hazard-form') }}" class="btn btn-square btn-primary btn-xs">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                         <path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
