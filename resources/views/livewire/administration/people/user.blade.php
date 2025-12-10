5de4a3f9beac7c068ea3a4f672211c8d83f6f71b
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-10 12:29:25 +0700

    asdfasf

--- resources/views/livewire/administration/people/user.blade.php
+++ resources/views/livewire/administration/people/user.blade.php
@@ -284,7 +284,7 @@
                 <x-label-error :messages="$errors->get('file')" />
 
                 {{-- Loading indicator saat pilih file --}}
-                <div wire:loading.class='block' wire:target="file" class="text-info text-sm mt-1 hidden">
+                <div wire:loading wire:target="file" wire:loading.class.remove="hidden" class="text-info text-sm mt-1 hidden">
                     ⏳ Sedang mengunggah file...
                 </div>
             </fieldset>
