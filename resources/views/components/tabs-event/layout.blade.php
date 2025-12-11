a589c6f456c741f2e4866eb569d725c8173b74bc
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-11 16:17:28 +0800

    ddsds

--- resources/views/components/tabs-event/layout.blade.php
+++ resources/views/components/tabs-event/layout.blade.php
@@ -1,5 +1,5 @@
 <div class="flex  flex-col ">
-     <div class=" overflow-x-auto md:w-80">
+     <div class=" overflow-x-auto md:w-80 md:overflow-x-hidden border-b border-contrast-lower mb-4">
         <flux:navlist-horizontal>
             <flux:navlist-horizontal.item :href="route('administration-event_general-eventCategory')" wire:navigate>{{ __('Event Category') }}</flux:navlist-horizontal.item>
             <flux:navlist-horizontal.item :href="route('administration-event_general-eventType')" wire:navigate>{{ __('Event Type') }}</flux:navlist-horizontal.item>
