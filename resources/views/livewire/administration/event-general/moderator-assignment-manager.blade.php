dc8e531d7def4b63cbc6516c9ce88272a768f74f
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-11 12:14:17 +0800

    asdf

--- resources/views/livewire/administration/event-general/moderator-assignment-manager.blade.php
+++ resources/views/livewire/administration/event-general/moderator-assignment-manager.blade.php
@@ -19,16 +19,13 @@
                         <input type="text" wire:model.live.debounce.300ms="searchModerator"
                             placeholder="Ketik untuk mencari dan memilih moderator..."
                            />
-                        <div wire:loading.delay wire:target="searchModerator" class="p-2 text-center">
+                        <div wire:loading.remove.class='hidden' wire:target="searchModerator" class="p-2 text-center hidden">
                             <span class="loading loading-spinner loading-sm text-secondary"></span>
                         </div>
                     </label>
                     @if ($showModeratorDropdown && count($users) > 0)
                         <ul
                             class="absolute z-10 bg-base-100 border rounded-md w-full mt-1 max-h-60 overflow-auto shadow">
-
-
-
                             @foreach ($users as $user)
                                 <li wire:click="selectModerator({{ $user->id }}, '{{ $user->name }}')"
                                     class="px-3 py-2 cursor-pointer hover:bg-base-200" wire:loading.attr="disabled">
