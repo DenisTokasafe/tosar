e15fa467f893a182cc0f7b1dc78ea3d43e4f6bec
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-11 15:18:52 +0800

    asdasdf

--- resources/views/livewire/administration/dept-group/group.blade.php
+++ resources/views/livewire/administration/dept-group/group.blade.php
@@ -39,57 +39,57 @@
                 </tfoot>
             </table>
         </div>
-        <flux:modal name="group">
-            <form wire:submit='store' class='grid justify-items-stretch'>
-                @csrf
-                <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs sm:w-sm border p-2 sm:justify-self-center">
-                    <legend class="fieldset-legend">{{ $legend }}</legend>
-                    {{-- nama group --}}
-                    <x-label-req>{{ __('nama grup') }} </x-label-req>
-                    <x-text-input wire:model.live='group_name' :error="$errors->get('group_name')" type="text" placeholder="group_name" />
-                    <x-label-error :messages="$errors->get('group_name')" />
-                </fieldset>
-
-                <div class="modal-action">
-                    <flux:button size="xs" type="submit" icon="save-icon" variant="primary">Save</flux:button>
-                    <flux:button size="xs" wire:click='close_modal' icon="close-icon" variant="danger">Close</flux:button>
-                </div>
-            </form>
-        </flux:modal>
-        <flux:modal name="group_edit">
-            <form wire:submit='store' class='grid justify-items-stretch'>
-                @csrf
-                <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs sm:w-sm border p-2 sm:justify-self-center">
-                    <legend class="fieldset-legend">{{ $legend }}</legend>
-                    {{-- nama group --}}
-                    <x-label-req>{{ __('nama grup') }} </x-label-req>
-                    <x-text-input wire:model.live='group_name' :error="$errors->get('group_name')" type="text" placeholder="group_name" />
-                    <x-label-error :messages="$errors->get('group_name')" />
-                </fieldset>
-
-                <div class="modal-action">
-                    <flux:button size="xs" type="submit" icon="save-icon" variant="primary">Save</flux:button>
-                    <flux:button size="xs" wire:click='close_modal' icon="close-icon" variant="danger">Close</flux:button>
-                </div>
-            </form>
-        </flux:modal>
-        <flux:modal name="delete-group" class="min-w-[22rem]">
-            <div class="space-y-6">
-                <div>
-                    <flux:heading size="lg">Delete project?</flux:heading>
-                    <flux:text class="mt-2">
-                        <p>You're about to delete this {{ $group_name }}.</p>
-                        <p>This action cannot be reversed.</p>
-                    </flux:text>
-                </div>
-                <div class="flex gap-2">
-                    <flux:spacer />
-                    <flux:modal.close>
-                        <flux:button variant="ghost">Cancel</flux:button>
-                    </flux:modal.close>
-                    <flux:button wire:click='delete' size='xs' variant="danger">Delete </flux:button>
-                </div>
-            </div>
-        </flux:modal>
     </x-tabs-group.layout>
+    <flux:modal name="group">
+        <form wire:submit='store' class='grid justify-items-stretch'>
+            @csrf
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs sm:w-sm border p-2 sm:justify-self-center">
+                <legend class="fieldset-legend">{{ $legend }}</legend>
+                {{-- nama group --}}
+                <x-label-req>{{ __('nama grup') }} </x-label-req>
+                <x-text-input wire:model.live='group_name' :error="$errors->get('group_name')" type="text" placeholder="group_name" />
+                <x-label-error :messages="$errors->get('group_name')" />
+            </fieldset>
+
+            <div class="modal-action">
+                <flux:button size="xs" type="submit" icon="save-icon" variant="primary">Save</flux:button>
+                <flux:button size="xs" wire:click='close_modal' icon="close-icon" variant="danger">Close</flux:button>
+            </div>
+        </form>
+    </flux:modal>
+    <flux:modal name="group_edit">
+        <form wire:submit='store' class='grid justify-items-stretch'>
+            @csrf
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs sm:w-sm border p-2 sm:justify-self-center">
+                <legend class="fieldset-legend">{{ $legend }}</legend>
+                {{-- nama group --}}
+                <x-label-req>{{ __('nama grup') }} </x-label-req>
+                <x-text-input wire:model.live='group_name' :error="$errors->get('group_name')" type="text" placeholder="group_name" />
+                <x-label-error :messages="$errors->get('group_name')" />
+            </fieldset>
+
+            <div class="modal-action">
+                <flux:button size="xs" type="submit" icon="save-icon" variant="primary">Save</flux:button>
+                <flux:button size="xs" wire:click='close_modal' icon="close-icon" variant="danger">Close</flux:button>
+            </div>
+        </form>
+    </flux:modal>
+    <flux:modal name="delete-group" class="min-w-[22rem]">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Delete project?</flux:heading>
+                <flux:text class="mt-2">
+                    <p>You're about to delete this {{ $group_name }}.</p>
+                    <p>This action cannot be reversed.</p>
+                </flux:text>
+            </div>
+            <div class="flex gap-2">
+                <flux:spacer />
+                <flux:modal.close>
+                    <flux:button variant="ghost">Cancel</flux:button>
+                </flux:modal.close>
+                <flux:button wire:click='delete' size='xs' variant="danger">Delete </flux:button>
+            </div>
+        </div>
+    </flux:modal>
 </section>
