13fff749d95b16acc93acb02917e3a81b940c02e
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-11 15:51:38 +0800

    adasd

--- resources/views/components/tabs-group/url_panel_group.blade.php
+++ resources/views/components/tabs-group/url_panel_group.blade.php
@@ -3,6 +3,15 @@
         <flux:button size="xs" wire:click='open_modal' icon="add-icon" variant="primary"></flux:button>
     </flux:tooltip>
     <div>
-        <flux:input size='xs' icon="magnifying-glass" wire:model.live='search_group' placeholder="Search Group..." />
+        <div class="relative">
+            <input type="text" wire:model.live.debounce.300ms="search_group"
+                placeholder="Ketik untuk mencari Departemen Group..." {{-- 💡 Terapkan SEMUA class styling ke input --}}
+                class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs pr-10" />
+            {{-- Spinner diposisikan absolute di kanan input --}}
+            <div wire:loading.remove.class='hidden' wire:target="search_group"
+                class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none z-10 hidden">
+                <span class="loading loading-spinner loading-sm text-secondary"></span>
+            </div>
+        </div>
     </div>
 </div>
