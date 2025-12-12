cfdc8afd5a1a8611b7031eddb6ade003c1b58156
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-12 10:18:26 +0800

    ASD

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,13 +32,15 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/private'),
+            'serve' => true,
             'throw' => false,
+            'report' => false,
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('app/public'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
