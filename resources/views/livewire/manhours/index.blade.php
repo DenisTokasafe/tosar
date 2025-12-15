ff387a525f3e66e1de2cd7fbb3277e436c3bba5d
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-15 15:32:13 +0800

    asdf

--- resources/views/livewire/manhours/index.blade.php
+++ resources/views/livewire/manhours/index.blade.php
@@ -10,9 +10,9 @@
         <div class="flex flex-row gap-2">
             @can('create', \App\Models\Manhour::class)
                 {{-- Tombol 'tambah data' --}}
-                <flux:tooltip content="tambah data" position="top">
+                {{-- <flux:tooltip content="tambah data" position="top">
                     <flux:button size="xs" wire:click='open_modal' icon="add-icon" variant="primary"></flux:button>
-                </flux:tooltip>
+                </flux:tooltip> --}}
 
                 {{-- Komponen Import --}}
                 @livewire('manhours.manhours-import')
