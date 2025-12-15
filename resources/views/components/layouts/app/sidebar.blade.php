41827c8440d267c71e943aed2fa9fa78004c29e6
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-15 15:24:56 +0800

    as

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -29,53 +29,7 @@
         <flux:spacer />
 
         {{-- Desktop User Menu: hanya tampil kalau user login --}}
-        @auth
-            <flux:dropdown class="hidden lg:block" position="bottom" align="start">
-                <flux:profile :name="auth()->user()->name" :initials="auth()->user()->initials()" icon:trailing="chevrons-up-down" />
 
-                <flux:menu class="w-[220px]">
-                    <flux:menu.radio.group>
-                        <div class="p-0 text-sm font-normal">
-                            <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
-                                <span class="relative flex w-8 h-8 overflow-hidden rounded-lg shrink-0">
-                                    <span class="flex items-center justify-center w-full h-full text-black rounded-lg bg-neutral-200 dark:bg-neutral-700 dark:text-white">
-                                        {{ auth()->user()->initials() }}
-                                    </span>
-                                </span>
-
-                                <div class="grid flex-1 text-sm leading-tight text-start">
-                                    <span class="font-semibold truncate">{{ auth()->user()->name }}</span>
-                                    <span class="text-xs truncate">{{ auth()->user()->email }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </flux:menu.radio.group>
-
-                    <flux:menu.separator />
-
-                    <flux:menu.radio.group>
-                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
-                    </flux:menu.radio.group>
-
-                    <flux:menu.separator />
-
-                    <form method="POST" action="{{ route('logout') }}" class="w-full">
-                        @csrf
-                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
-                            {{ __('Log Out') }}
-                        </flux:menu.item>
-                    </form>
-                </flux:menu>
-            </flux:dropdown>
-        @else
-            {{-- Desktop guest actions (login/register) --}}
-            <div class="items-center hidden gap-2 lg:flex">
-                <a href="{{ route('login') }}" class="btn btn-outline btn-xs">{{ __('Login') }}</a>
-                @if(Route::has('register'))
-                    <a href="{{ route('register') }}" class="btn btn-outline btn-xs">{{ __('Register') }}</a>
-                @endif
-            </div>
-        @endauth
 
     </flux:sidebar>
 
