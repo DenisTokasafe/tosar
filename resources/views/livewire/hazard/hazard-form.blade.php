48b283fa0951a39973f14257d584749951916b02
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-15 11:28:30 +0800

    sdasdf

--- resources/views/livewire/hazard/hazard-form.blade.php
+++ resources/views/livewire/hazard/hazard-form.blade.php
@@ -90,7 +90,7 @@
                             <span class="text-xs text-info">Mengunggah...</span>
                         </span>
                         <!-- Nama file -->
-                       <span id="file-name-corrective" class="text-xs text-gray-500" wire:loading.remove
+                       <span id="file-name" class="text-xs text-gray-500" wire:loading.remove
                             wire:target="doc_deskripsi">
                             {!! $doc_deskripsi ? $doc_deskripsi->getClientOriginalName() : 'Belum ada file' !!}
                         </span>
