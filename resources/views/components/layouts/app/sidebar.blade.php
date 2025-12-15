4daeda231349da9ec131e19d246271cbfff6f9e1
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-15 15:15:07 +0800

    asfasdf

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html data-theme="corporate" lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<html data-theme="corporate" lang="{{ str_replace('_', '-', app()->getLocale()) }}" >
 <head>
     {{-- @laravelPWA --}}
     @include('partials.head')
@@ -37,15 +37,15 @@
                     <flux:menu.radio.group>
                         <div class="p-0 text-sm font-normal">
                             <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
-                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                                    <span class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                <span class="relative flex w-8 h-8 overflow-hidden rounded-lg shrink-0">
+                                    <span class="flex items-center justify-center w-full h-full text-black rounded-lg bg-neutral-200 dark:bg-neutral-700 dark:text-white">
                                         {{ auth()->user()->initials() }}
                                     </span>
                                 </span>
 
-                                <div class="grid flex-1 text-start text-sm leading-tight">
-                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
-                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                                <div class="grid flex-1 text-sm leading-tight text-start">
+                                    <span class="font-semibold truncate">{{ auth()->user()->name }}</span>
+                                    <span class="text-xs truncate">{{ auth()->user()->email }}</span>
                                 </div>
                             </div>
                         </div>
@@ -69,7 +69,7 @@
             </flux:dropdown>
         @else
             {{-- Desktop guest actions (login/register) --}}
-            <div class="hidden lg:flex items-center gap-2">
+            <div class="items-center hidden gap-2 lg:flex">
                 <a href="{{ route('login') }}" class="btn btn-outline btn-xs">{{ __('Login') }}</a>
                 @if(Route::has('register'))
                     <a href="{{ route('register') }}" class="btn btn-outline btn-xs">{{ __('Register') }}</a>
@@ -79,8 +79,8 @@
 
     </flux:sidebar>
 
-    <flux:header sticky class=" shadow-md max-lg:hidden bg-base-300">
-        <div class="cursor-pointer group hover:bg-zinc-700 rounded-lg px-2 block lg:h-0 lg:hidden" x-on:click="sidebarHidden = !sidebarHidden">
+    <flux:header sticky class="shadow-md max-lg:hidden bg-base-300">
+        <div class="block px-2 rounded-lg cursor-pointer group hover:bg-zinc-700 lg:h-0 lg:hidden" x-on:click="sidebarHidden = !sidebarHidden">
             <svg class="w-4 h-4 stroke-gray-400 group-hover:stroke-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
                 <line x1="9" y1="3" x2="9" y2="21" />
@@ -92,13 +92,13 @@
 
             <!-- hamburger icon -->
 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 hidden lg:block swap-off fill-current">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="hidden fill-current size-4 lg:block swap-off">
                 <path fill-rule="evenodd" d="M2 3.75A.75.75 0 0 1 2.75 3h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 3.75ZM2 8a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 8Zm0 4.25a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
             </svg>
 
             <!-- close icon -->
 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 hidden lg:block swap-on fill-current">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="hidden fill-current size-4 lg:block swap-on">
                 <path fill-rule="evenodd" d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
             </svg>
         </label>
@@ -119,15 +119,15 @@
                     <flux:menu.radio.group>
                         <div class="p-0 text-sm font-normal">
                             <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
-                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                                    <span class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                <span class="relative flex w-8 h-8 overflow-hidden rounded-lg shrink-0">
+                                    <span class="flex items-center justify-center w-full h-full text-black rounded-lg bg-neutral-200 dark:bg-neutral-700 dark:text-white">
                                         {{ auth()->user()->initials() }}
                                     </span>
                                 </span>
 
-                                <div class="grid flex-1 text-start text-sm leading-tight">
-                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
-                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                                <div class="grid flex-1 text-sm leading-tight text-start">
+                                    <span class="font-semibold truncate">{{ auth()->user()->name }}</span>
+                                    <span class="text-xs truncate">{{ auth()->user()->email }}</span>
                                 </div>
                             </div>
                         </div>
@@ -161,8 +161,8 @@
 
     {{ $slot }}
 
-    @fluxScripts
     @livewireScripts
     @stack('scripts')
+    @fluxScripts
 </body>
 </html>
