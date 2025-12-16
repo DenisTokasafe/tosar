ccc7d758fb2af900690beffc1b607fd01785f1de
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-16 12:59:12 +0800

    asdasdasd

--- resources/views/livewire/hazard/hazard-detail.blade.php
+++ resources/views/livewire/hazard/hazard-detail.blade.php
@@ -789,7 +789,7 @@
                                                                 placeholder="Masukkan nama..."
                                                                 class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs {{ $errors->has('manualPelaporName') ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '' }}" />
                                                             <div class="absolute right-0 -translate-y-1/2 top-1/2">
-                                                                <flux:button  size="xs"  class="{{ $isDisabled ? 'btn btn-disabled cursor-not-allowed' : '' }}"
+                                                                <flux:button  size="xs"
                                                                     wire:click="addActPelaporManual" icon="plus"
                                                                     variant="primary">
                                                                     Tambah
@@ -810,7 +810,7 @@
                             </div>
                             <!-- Tombol Tambah -->
                             <div class="flex justify-end ">
-                                <flux:button size="xs" wire:click="addActionHazard" icon:trailing="add-icon"
+                                <flux:button size="xs" wire:click="addActionHazard"  class="{{ $isDisabled ? 'btn btn-disabled cursor-not-allowed' : '' }}" icon:trailing="add-icon"
                                     variant="primary">Tambah</flux:button>
                             </div>
                             <!-- List Actions -->
