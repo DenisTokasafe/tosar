02f6839e6526bd3ce7cf7d333993ae7a384b7d1e
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-16 08:15:32 +0800

    eweww

--- resources/views/flux/menu/index.blade.php
+++ resources/views/flux/menu/index.blade.php
@@ -2,8 +2,8 @@
 $classes = Flux::classes()
 ->add('[:where(&)]:min-w-48 p-[.3125rem]')
 ->add('rounded-sm shadow-xs')
-->add('border border-zinc-200 dark:border-base-100')
-->add('bg-white dark:bg-base-200')
+->add('border-base-100')
+->add('bg-base-200')
 ->add('focus:outline-hidden')
 ;
 @endphp
