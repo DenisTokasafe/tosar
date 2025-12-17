8605ca1fc3d28a3e5e0af80a8bb76deb5d3ab5e1
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-17 09:21:24 +0800

    sa

--- config/ldap.php
+++ config/ldap.php
@@ -26,17 +26,18 @@ return [
     |
     */
 
-'connections' => [
-    'default' => [
-        'hosts'    => ['adserver.archimining.local'], // atau IP
-        'username' => null,   // <- kosong, tidak pakai service account
-        'password' => null,
-        'port'     => 636,    // 636 jika LDAPS
-        'use_ssl'  => true,
-        'use_tls'  => false,
-        'base_dn'  => 'DC=archimining,DC=local',
+    'connections' => [
+        'default' => [
+            'hosts' => ['arctktad006.archimining.local'],
+            'base_dn' => 'dc=archimining,dc=com',
+            'username' => null,   // <- kosong, tidak pakai service account
+            'password' => null,
+            'port'     => 636,    // 636 jika LDAPS
+            'use_ssl'  => true,
+            'use_tls'  => false,
+            'base_dn'  => 'DC=archimining,DC=local',
+        ],
     ],
-],
 
 
     /*
