b1b372b10d6537626766713243c08417ef57f428
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-17 08:21:37 +0800

    dsds

--- app/Http/Controllers/HazardController.php
+++ app/Http/Controllers/HazardController.php
@@ -19,7 +19,9 @@ class HazardController extends Controller
         'pelapor',
         'location',
         'consequence',
-        'likelihood'
+        'likelihood',
+        'hazardKondisiTidakAman',
+        'hazardTindakanTidakAman',
     ])->get();
 
     // Mapping data agar lebih ringkas dan mudah dibaca (flattened structure)
@@ -43,8 +45,8 @@ class HazardController extends Controller
             'lokasi_spesifik' => $hazard->location_specific,
             'tindakan_korektif_segera' => $hazard->immediate_corrective_action,
             'kata_kunci' => $hazard->key_word,
-            'kondisi_tidak_aman_id' => $hazard->kondisi_tidak_aman_id ? $hazard->hazardKondisiTidakAman->name : null,
-            'tindakan_tidak_aman_id' => $hazard->tindakan_tidak_aman_id ? $hazard->hazardTindakanTidakAman->name : null,
+            'kondisi_tidak_aman_id' => $hazard->hazardKondisiTidakAman->name ?? null,
+            'tindakan_tidak_aman_id' => $hazard->hazardTindakanTidakAman->name ?? null,
             // ... tambahkan semua kolom yang Anda butuhkan
         ];
     });
