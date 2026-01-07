582b12c7e2eafa372274cc0341b8e6c7470982d9
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-07 10:32:56 +0800

    asdfad

--- resources/views/livewire/hazard/hazard-form.blade.php
+++ resources/views/livewire/hazard/hazard-form.blade.php
@@ -420,10 +420,11 @@
                     </fieldset>
                     <!-- Dilaporkan Oleh -->
                     <x-form.searchable-select-advanced label="Dilaporkan Oleh" placeholder="Cari Nama Pelapor..."
-                        modelsearch="searchActResponsibility" modelid="responsible_id" {{-- ID asli di DB --}}
-                        :options="$pelaporsAct" :showdropdown="$show_act_elapor_dropdown" {{-- Logic Manual --}} :manualmode="$manual_act_pelapor_mode"
-                        manualmodelname="manualActPelaporName" enablemanualaction="enableManualActPelapor"
-                        addmanualaction="addActPelaporManual" clickaction="selectActPelapor" columnname="name" />
+                        modelsearch="searchActResponsibility" modelid="action_responsible_id" {{-- Gunakan action_responsible_id sesuai class --}}
+                        :options="$pelaporsAct" :showdropdown="$show_act_pelapor_dropdown" {{-- Nama variabel harus sama dengan di class --}} :manualmode="$manualActPelaporMode"
+                        {{-- Nama variabel harus sama dengan di class --}} manualmodelname="manualActPelaporName"
+                        enablemanualaction="enableManualActPelapor" addmanualaction="addActPelaporManual"
+                        clickaction="selectActPelapor" columnname="name" />
                 </div>
 
                 <!-- Tombol Tambah -->
