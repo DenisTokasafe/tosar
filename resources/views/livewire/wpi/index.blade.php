83d43b90f1ec6416b4937d4490664ccd39aee33f
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-09 15:20:05 +0800

    asdf

--- resources/views/livewire/wpi/index.blade.php
+++ resources/views/livewire/wpi/index.blade.php
@@ -141,7 +141,7 @@
                     </button>
                 </div>
 
-                <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+                <div class="grid grid-cols-1 gap-4 bg-blue-600 md:grid-cols-2">
                     @foreach ($inspectors as $index => $inspector)
                         <div class="flex items-start space-x-2" wire:key="ins-{{ $index }}">
                             <span class="mt-2 text-xs font-bold text-gray-400">{{ $index + 1 }}.</span>
