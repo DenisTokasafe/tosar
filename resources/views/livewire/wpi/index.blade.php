c48d3e863eb8687d93fff9c76846eca1cb086db5
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-09 14:02:28 +0800

    adfasdfa

--- resources/views/livewire/wpi/index.blade.php
+++ resources/views/livewire/wpi/index.blade.php
@@ -104,7 +104,7 @@
 
                             <div class="hidden peer-checked/department:block">
                                 {{-- Department --}}
-                                <div class="relative ">
+                                <div class="relative mb-1 ">
                                     <x-form.searchable-dropdown-without-label modelsearch="search"
                                         modelid="department_id" placeholder="Cari Departemen..." :options="$departments"
                                         :showdropdown="$showDropdown" clickaction="selectDepartment" namedb="department_name" />
@@ -112,7 +112,7 @@
                             </div>
                             <div class="hidden peer-checked/company:block">
                                 {{-- Contractor --}}
-                                <div class="relative ">
+                                <div class="relative mb-1 ">
                                     <x-form.searchable-dropdown-without-label modelsearch="searchContractor"
                                         placeholder="Cari Kontraktor..." modelid="contractor_id" :options="$contractors"
                                         :showdropdown="$showContractorDropdown" clickaction="selectContractor" namedb="contractor_name" />
