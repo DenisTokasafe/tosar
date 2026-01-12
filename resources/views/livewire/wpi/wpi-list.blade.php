4c234f8d68b8ec7e2c7474175b7d3ccfdd6f5f2e
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-12 12:07:50 +0800

    asdf

--- resources/views/livewire/wpi/wpi-list.blade.php
+++ resources/views/livewire/wpi/wpi-list.blade.php
@@ -12,7 +12,7 @@
                 placeholder="Cari Departemen..."
                 class="w-64 input input-bordered input-sm focus:ring-info" />
 
-            <a href="/wpi/form" class="text-xs uppercase btn btn-primary btn-sm">
+            <a href="/wpi/create" class="text-xs uppercase btn btn-primary btn-sm">
                 + Laporan Baru
             </a>
         </div>
@@ -43,7 +43,7 @@
                         </td>
                         <td class="flex justify-center gap-2 p-3">
                             {{-- Action Edit: Mengarah ke form dengan ID --}}
-                            <a href="/wpi/form/{{ $report->id }}" class="text-blue-500 btn btn-square btn-ghost btn-xs hover:bg-blue-50" title="Edit">
+                            <a href="/wpi/edit/{{ $report->id }}" class="text-blue-500 btn btn-square btn-ghost btn-xs hover:bg-blue-50" title="Edit">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                 </svg>
