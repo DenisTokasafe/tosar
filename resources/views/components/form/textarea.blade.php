2c79d4e7fcb59c7375afa2efbd514674c00b0fbc
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-12 09:56:38 +0800

    asdasd

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -12,7 +12,7 @@
             {{ $model ? "wire:model.live=$model" : '' }}
             placeholder="{{ $placeholder ?: $label }}"
             rows="{{ $rows }}"
-            {{ $attributes->merge(['class' => "textarea textarea-bordered w-full $size border-gray-300 rounded focus:border-info focus:ring-info".($errors->has($model) ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '')]) }}
+            {{ $attributes->merge(['class' => "textarea textarea-bordered focus:border-info focus:ring-info focus:outline-hidden w-full $size border-gray-300 rounded ".($errors->has($model) ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '')]) }}
         ></textarea>
         @if($label)
             <span>{{ $label }}</span>
