790580ac7789c773c93f6bfa47837006824eefd0
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-13 13:05:51 +0800

    sas

--- resources/views/livewire/wpi/wpi-list.blade.php
+++ resources/views/livewire/wpi/wpi-list.blade.php
@@ -1,21 +1,18 @@
 <section class="w-full">
     <x-toast />
-    <x-tabs-wpi.layout heading="Daftar Laporan WPI" subheading="Work Permit Inspection - KPLH Site Tokatindung">
-        <div class="flex flex-col items-center justify-between gap-4 mb-6 md:flex-row">
-            {{-- <div>
-                <h1 class="text-xl font-bold tracking-wider text-gray-800 uppercase">Daftar Laporan WPI</h1>
-                <p class="text-xs italic text-gray-500">Work Permit Inspection - KPLH Site Tokatindung</p>
-            </div> --}}
+    <div class="flex flex-col items-center justify-between gap-4 mb-6 md:flex-row">
+        {{-- <div>
+            <h1 class="text-xl font-bold tracking-wider text-gray-800 uppercase">Daftar Laporan WPI</h1>
+            <p class="text-xs italic text-gray-500">Work Permit Inspection - KPLH Site Tokatindung</p>
+        </div> --}}
 
-            <div class="flex items-center gap-3">
-                {{-- <input type="text" wire:model.live.debounce.300ms="search" placeholder="Cari Departemen..."
-                    class="w-64 input input-bordered input-sm focus:ring-info" /> --}}
-
-                <a href="{{ route('wpi.create') }}" class="text-xs uppercase btn btn-primary btn-sm">
-                    + Laporan Baru
-                </a>
-            </div>
+        <div class="flex items-center gap-3">
+            <a href="{{ route('wpi.create') }}" class="text-xs uppercase btn btn-primary btn-sm">
+                + Laporan Baru
+            </a>
         </div>
+    </div>
+    <x-tabs-wpi.layout heading="Daftar Laporan WPI" subheading="Work Permit Inspection - KPLH Site Tokatindung">
 
         <div class="overflow-x-auto">
             <table class="table w-full border border-collapse border-gray-200 table-xs">
