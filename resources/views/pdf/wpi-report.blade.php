b4bf6483638c27369558a68e2cb8023bc66549d5
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-13 08:17:42 +0800

    sas

--- resources/views/pdf/wpi-report.blade.php
+++ resources/views/pdf/wpi-report.blade.php
@@ -4,16 +4,16 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <style>
         @page {
-            /* Ruang untuk header (100px) dan footer (110px) */
-            margin: 100px 1cm 110px 1cm;
+            /* Margin yang cukup untuk header & footer agar tidak tertimpa isi main */
+            margin: 110px 1cm 110px 1cm;
         }
 
         header {
             position: fixed;
-            top: -85px;
+            top: -95px;
             left: 0;
             right: 0;
-            height: 80px;
+            height: 90px;
         }
 
         footer {
@@ -32,46 +32,44 @@
             line-height: 1.1;
         }
 
-        .header-table, .main-table, .footer-table {
+        .main-table, .footer-table, .header-table {
             width: 100%;
             border-collapse: collapse;
         }
 
-        /* Border hitam tegas sesuai gambar */
-        .header-table td, .main-table th, .main-table td, .footer-table td {
+        .main-table td, .main-table th, .footer-table td, .header-table td {
             border: 1px solid #000;
-            padding: 2px 5px;
-            vertical-align: top;
-            word-wrap: break-word;
+            padding: 2px 4px;
+            vertical-align: middle;
         }
-
-        .header-table td { vertical-align: middle; }
 
         .bg-gray {
-            background-color: #e2e8f0;
-            font-weight: normal;
+            background-color: #ffffff; /* Sesuai gambar, background putih bersih */
         }
 
-        .center { text-align: center; }
+        /* Styling teks bilingual (Biru Muda/Italic untuk Inggris) */
+        .eng-text {
+            color: #1e40af;
+            font-style: italic;
+        }
 
-        /* Teks Merah Footer sesuai gambar */
+        /* Styling teks merah di tengah footer */
         .red-warning {
             color: #ff0000;
             font-weight: bold;
-            font-style: italic;
-            font-size: 8pt;
+            font-size: 10pt; /* Lebih besar sesuai gambar */
         }
 
-        img.photo {
-            width: 130px;
-            height: auto;
-            margin: 3px;
-            border: 0.5px solid #000;
-        }
+        .center { text-align: center; }
+        .right { text-align: right; }
 
-        /* Penomoran Halaman Otomatis */
+        /* Penomoran Halaman */
         .pagenum:before { content: counter(page); }
         .totalpage:before { content: counter(pages); }
+
+        /* Khusus untuk Main Content */
+        main .main-table td { vertical-align: top; }
+        img.photo { width: 130px; height: auto; margin: 3px; border: 0.5px solid #000; }
     </style>
 </head>
 <body>
@@ -95,66 +93,66 @@
     </header>
 
     <footer>
-        <table class="footer-table" style="font-size: 8pt;">
+        <table class="footer-table">
             <tr>
-                <td width="20%" class="bg-gray">Nama Dokumen</td>
-                <td width="30%">Formulir Laporan WPI KPLH</td>
-                <td width="20%" class="bg-gray">Tanggal Terbit</td>
-                <td width="30%">15 Maret 2023</td>
+                <td width="30%">Nama Dokumen/<span class="eng-text">Document Name</span></td>
+                <td width="70%" colspan="3">Formulir Laporan WPI KPLH</td>
             </tr>
             <tr>
-                <td class="bg-gray">Ditetapkan Oleh</td>
-                <td>Kepala Teknik Tambang</td>
-                <td class="bg-gray">Tanggal Tinjau Ulang</td>
-                <td>15 Maret 2026</td>
+                <td width="30%">Ditetapkan Oleh/<span class="eng-text">Determined By</span></td>
+                <td width="35%">Kepala Teknik Tambang/<span class="eng-text">Mining Technical Head</span></td>
+                <td width="20%">Tanggal Terbit /<span class="eng-text">Date of Issue</span></td>
+                <td width="15%">15 Maret 2023</td>
             </tr>
             <tr>
-                <td class="bg-gray">No Dokumen</td>
-                <td>TT-MGT-FRS-024A</td>
-                <td colspan="2" rowspan="2" class="center" style="vertical-align: middle; border-bottom: none;">
+                <td width="30%">No Dokumen/<span class="eng-text">No Document</span></td>
+                <td width="35%">TT-MGT-FRS-024A</td>
+                <td width="20%">Tanggal Tinjau Ulang / <span class="eng-text">Review Date</span></td>
+                <td width="15%">15 Maret 2026</td>
+            </tr>
+            <tr>
+                <td width="12%">No Revisi</td>
+                <td width="8%" class="center">00</td>
+                <td width="55%" class="center">
                     <span class="red-warning">Dokumen terkendali dan valid hanya ada di sharepoint Archi Indonesia</span>
                 </td>
-            </tr>
-            <tr>
-                <td class="bg-gray" style="border-bottom: 1px solid #000;">No Revisi</td>
-                <td class="center" style="border-bottom: 1px solid #000;">00</td>
+                <td width="25%" class="right">
+                    <strong>Halaman <span class="pagenum"></span> dari <span class="totalpage"></span></strong>
+                </td>
             </tr>
         </table>
-        <div style="text-align: right; font-size: 9pt; margin-top: -15px; font-weight: bold; background: white; width: 120px; float: right; position: relative; z-index: 10;">
-            Halaman <span class="pagenum"></span> dari <span class="totalpage"></span>
-        </div>
     </footer>
 
     <main>
         <table class="main-table" style="margin-bottom: 10px;">
-            <tr>
-                <td width="15%" class="bg-gray">Tanggal /Date</td>
+            <tr style="background-color: #e2e8f0; font-weight: bold;">
+                <td width="15%">Tanggal /Date</td>
                 <td width="30%">{{ date('d F Y', strtotime($report->report_date)) }}</td>
-                <td width="25%" class="bg-gray center">Nama Petugas Inspeksi/Inspector</td>
-                <td width="10%" class="bg-gray center">ID</td>
-                <td width="20%" class="bg-gray center">Department/Contractor</td>
+                <td width="25%" class="center">Nama Petugas Inspeksi/Inspector</td>
+                <td width="10%" class="center">ID</td>
+                <td width="20%" class="center">Department/Contractor</td>
             </tr>
 
             @php $maxRows = 6; @endphp
             @for ($i = 0; $i < $maxRows; $i++)
             <tr>
                 @if ($i == 0)
-                    <td class="bg-gray">Jam /Time</td>
+                    <td style="background-color: #e2e8f0; font-weight: bold;">Jam /Time</td>
                     <td>{{ $report->report_time }}</td>
                 @elseif ($i == 1)
-                    <td class="bg-gray">Lokasi/Location</td>
+                    <td style="background-color: #e2e8f0; font-weight: bold;">Lokasi/Location</td>
                     <td>{{ $report->locationRelation->name ?? '-' }}</td>
                 @elseif ($i == 2)
-                    <td class="bg-gray">Site Name</td>
+                    <td style="background-color: #e2e8f0; font-weight: bold;">Site Name</td>
                     <td>Tokatindung</td>
                 @elseif ($i == 3)
-                    <td class="bg-gray">Area</td>
+                    <td style="background-color: #e2e8f0; font-weight: bold;">Area</td>
                     <td>Mining</td>
                 @elseif ($i == 4)
-                    <td class="bg-gray">Company</td>
+                    <td style="background-color: #e2e8f0; font-weight: bold;">Company</td>
                     <td>PT MSM</td>
                 @elseif ($i == 5)
-                    <td class="bg-gray">Department</td>
+                    <td style="background-color: #e2e8f0; font-weight: bold;">Department</td>
                     <td>{{ $report->department }}</td>
                 @endif
 
@@ -164,9 +162,9 @@
             </tr>
             @endfor
             <tr>
-                <td class="bg-gray">Contractor</td>
+                <td style="background-color: #e2e8f0; font-weight: bold;">Contractor</td>
                 <td>{{ $report->searchContractor ?? '-' }}</td>
-                <td class="bg-gray center">Direview oleh/Reviewing by:</td>
+                <td style="background-color: #e2e8f0; font-weight: bold;" class="center">Direview oleh/Reviewing by:</td>
                 <td colspan="2" class="center">
                     <div style="height: 25px;"></div>
                     <strong>MARDIN</strong><br>
@@ -177,7 +175,7 @@
 
         <table class="main-table">
             <thead>
-                <tr class="bg-gray center">
+                <tr style="background-color: #e2e8f0; font-weight: bold;" class="center">
                     <th width="4%">No</th>
                     <th width="6%">OHS Risk</th>
                     <th width="30%">Uraian Tindakan Tidak Aman / Kondisi Tidak Aman</th>
@@ -186,7 +184,7 @@
                 </tr>
             </thead>
             <tbody>
-                @forelse($report->findings as $index => $find)
+                @foreach($report->findings as $index => $find)
                 <tr>
                     <td class="center">{{ $index + 1 }}</td>
                     <td class="center">{{ $find->ohs_risk }}</td>
@@ -212,11 +210,7 @@
                         <strong>Selesai:</strong> {{ $find->completion_date ? date('d/m/y', strtotime($find->completion_date)) : '-' }}
                     </td>
                 </tr>
-                @empty
-                <tr>
-                    <td colspan="5" class="italic center">Tidak ada temuan.</td>
-                </tr>
-                @endforelse
+                @endforeach
             </tbody>
         </table>
     </main>
