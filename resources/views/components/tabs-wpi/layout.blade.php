a37b4bc093c10d90cc8308bb4461364570d928af
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 07:41:49 +0800

    daa

--- resources/views/components/tabs-wpi/layout.blade.php
+++ resources/views/components/tabs-wpi/layout.blade.php
@@ -1,8 +1,5 @@
 
-@props([
-    'id' => null,
 
-])
 <div class="z-30 flex max-md:flex-col ">
     <div class="self-stretch flex-1 max-md:pt-6">
         <div wire:ignore class="flex flex-col items-center justify-between gap-4 mb-6 md:flex-row">
