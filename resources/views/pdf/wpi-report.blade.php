a656eb977344b0fbb30284acc8e4c1c3de8ac2ef
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 12:34:59 +0800

    asdf

--- resources/views/pdf/wpi-report.blade.php
+++ resources/views/pdf/wpi-report.blade.php
@@ -234,15 +234,15 @@
             </tr>
             <tr>
                 <td class="border-l-none">Ditetapkan Oleh/<span class="en">Determined By</span></td>
-                <td colspan="2">Kepala Teknik Tambang/<span class="en">Mining Technical Head</span></td>
+                <td >Kepala Teknik Tambang/<span class="en">Mining Technical Head</span></td>
                 <td >Tanggal Terbit / <span class="en">Date of Issue</span></td>
                 <td class="border-r-none">15-03-2023</td>
             </tr>
             <tr>
                 <td class="border-l-none">No Dokumen/<span class="en">No Document</span></td>
-                <td colspan="2">TT-MGT-FRS-024A</td>
+                <td >TT-MGT-FRS-024A</td>
                 <td >Tanggal Peninjauan / <span class="en">Review Date</span></td>
-                <td class="border-r-none">15-03-2026</td>
+                <td >15-03-2026</td>
             </tr>
             <tr>
                 <td class="border-l-none">No Revisi</td>
