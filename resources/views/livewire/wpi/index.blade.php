790b5c602cea3711f58f4f4e983dce3d80af70bf
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 15:43:44 +0800

    afaf

--- resources/views/livewire/wpi/index.blade.php
+++ resources/views/livewire/wpi/index.blade.php
@@ -490,7 +490,7 @@
                                             @endforeach
                                         @endif
                                     </div>
-                                    <<fieldset class="relative fieldset">
+                                    <fieldset class="relative fieldset">
                                         <x-form.label label="Tanggal Jatuh Tempo:" required />
                                         <div
                                             class="{{ $errors->has('findings.' . $index . '.due_date') ? 'ring-1 ring-rose-500 rounded' : 'ring-base-300 rounded' }}">
