300a7a1b5a39b06c2b3ed8295a0c031db68279cb
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 10:41:27 +0800

    ASDASD

--- resources/views/pdf/wpi-report.blade.php
+++ resources/views/pdf/wpi-report.blade.php
@@ -179,8 +179,7 @@
         <table class="footer-table">
             <tr>
                 <td width="20%">Nama Dokumen/<span class="en">Document Name</span></td>
-                <td colspan="3" >Formulir Laporan WPI KPLH</td>
-
+                <td  width="10%" colspan="3" >Formulir Laporan WPI KPLH</td>
             </tr>
             <tr>
                 <td>Ditetapkan Oleh/<span class="en">Determined By</span></td>
