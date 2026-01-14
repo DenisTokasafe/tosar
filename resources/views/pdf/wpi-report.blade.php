f7b2a7470da7e8aa4938706d19d96815e5d7db32
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 11:04:20 +0800

    sas

--- resources/views/pdf/wpi-report.blade.php
+++ resources/views/pdf/wpi-report.blade.php
@@ -54,7 +54,7 @@
 
         /* Teks footer tetap 8px */
         .footer-table td {
-            font-size: 8px !important;
+            font-size: 7px !important;
         }
 
         /* Main table tetap menggunakan border hitam pekat (#000) */
