94041e3bce632127a8ee225c4da74f8852bb987d
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 10:20:09 +0800

    sa

--- resources/views/pdf/wpi-report.blade.php
+++ resources/views/pdf/wpi-report.blade.php
@@ -178,19 +178,19 @@
     <footer>
         <table class="footer-table">
             <tr>
-                <td width="20%">Nama Dokumen/<br><span class="en">Document Name</span></td>
+                <td width="20%">Nama Dokumen/<span class="en">Document Name</span></td>
                 <td colspan="3">Formulir Laporan WPI KPLH</td>
             </tr>
             <tr>
-                <td width="20%">Ditetapkan Oleh/<br><span class="en">Determined By</span></td>
-                <td width="30%">Kepala Teknik Tambang/<br><span class="en">Mining Technical Head</span></td>
-                <td width="25%">Tanggal Terbit/<br><span class="en">Date of Issue</span></td>
+                <td width="20%">Ditetapkan Oleh/<span class="en">Determined By</span></td>
+                <td width="30%">Kepala Teknik Tambang/<span class="en">Mining Technical Head</span></td>
+                <td width="25%">Tanggal Terbit/<span class="en">Date of Issue</span></td>
                 <td width="25%">15 Maret 2023</td>
             </tr>
             <tr>
-                <td>No Dokumen/<br><span class="en">No Document</span></td>
+                <td>No Dokumen/<span class="en">No Document</span></td>
                 <td>TT-MGT-FRS-024A</td>
-                <td>Tanggal Tinjau Ulang/<br><span class="en">Review Date</span></td>
+                <td>Tanggal Tinjau Ulang/<span class="en">Review Date</span></td>
                 <td>15 Maret 2026</td>
             </tr>
             <tr>
