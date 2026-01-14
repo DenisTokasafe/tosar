7417810f4b634bf67fc35deefc635b664d4f2526
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 15:21:12 +0800

    asdf

--- resources/views/livewire/wpi/index.blade.php
+++ resources/views/livewire/wpi/index.blade.php
@@ -190,15 +190,12 @@
                             {{-- Tombol Remove --}}
                             <div class="flex-none w-14">
                                 @if (count($inspectors) > 1)
-                                    <button type="button" wire:click="removeInspector({{ $index }})"
-                                        class="mt-2 text-red-500">
-                                        <svg class="w-4 h-4" fill="none" stroke="currentColor"
-                                            viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
-                                            </path>
-                                        </svg>
-                                    </button>
+                                     {{-- Tombol Hapus --}}
+                                    <flux:tooltip content="hapus" position="top">
+                                        <flux:button wire:click="removeInspector({{ $index }})"
+                                            size="xs" icon="trash" variant="danger">
+                                        </flux:button>
+                                    </flux:tooltip>
                                 @endif
                             </div>
                         </div>
