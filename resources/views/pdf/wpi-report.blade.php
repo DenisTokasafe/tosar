6eb70343f87e4cdd02a8edd5bafcd9108867d6b0
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 09:57:21 +0800

    sd

--- resources/views/pdf/wpi-report.blade.php
+++ resources/views/pdf/wpi-report.blade.php
@@ -178,14 +178,14 @@
     <footer>
         <table class="footer-table">
             <tr>
-                <td width="25%">Nama Dokumen/<span class="en">Document Name</span></td>
+                <td>Nama Dokumen/<span class="en">Document Name</span></td>
                 <td colspan="3" width="30%">Formulir Laporan WPI KPLH</td>
 
             </tr>
             <tr>
                 <td>Ditetapkan Oleh/<span class="en">Determined By</span></td>
                 <td>Kepala Teknik Tambang/<span class="en">Mining Technical Head</span></td>
-                <td width="25%">Tanggal Terbit/<span class="en">Date of Issue</span></td>
+                <td>Tanggal Terbit/<span class="en">Date of Issue</span></td>
                 <td>15 Maret 2023</td>
 
             </tr>
@@ -202,7 +202,7 @@
                 <td colspan="2" class="red-note">
                     Dokumen terkendali dan valid hanya ada di SharePoint Archi Indonesia
                 </td>
-                <td width="25%" class="right"></td>
+                <td class="right"></td>
             </tr>
         </table>
     </footer>
