2063dd2777f17c80601059a3790d943b7ff5ec15
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-14 15:46:03 +0800

    sa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
         // RiskMatrixCellSeeder::class,
         // UserSeeder::class,
-        HazardWorkflowSeeder::class
+        // HazardWorkflowSeeder::class
+        WpiWorkflowSeeder::class,
     ]);
     }
 }
