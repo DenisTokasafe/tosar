75f297e2afb7180bf4f69d079ac5cea45f68de06
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-15 11:24:03 +0800

    asdf

--- resources/views/components/tabs-wpi/layout.blade.php
+++ resources/views/components/tabs-wpi/layout.blade.php
@@ -1,7 +1,7 @@
 @if (Route::is('wpi.list') || Route::is('wpi.create'))
-    <div class="z-30 flex max-md:flex-col ">
+    <div wire:ignore class="z-30 flex max-md:flex-col ">
         <div class="self-stretch flex-1 max-md:pt-6">
-            <div wire:ignore class="flex flex-col items-center justify-between gap-4 mb-6 md:flex-row">
+            <div  class="flex flex-col items-center justify-between gap-4 mb-6 md:flex-row">
                 <div>
                     <flux:heading>{{ $heading ?? '' }}</flux:heading>
                     <flux:subheading size='sm'>{{ $subheading ?? '' }}</flux:subheading>
