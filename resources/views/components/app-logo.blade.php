5753ff05c301d80582fcbe1d3b72d1b04e53b0bb
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-15 15:30:59 +0800

    asasas

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,7 +2,7 @@
     class="flex items-center justify-center rounded-full aspect-square size-9 bg-accent-content text-accent-foreground">
     <div class="avatar">
         <div class="w-24 rounded-full">
-            <x-app-logo-icon class="text-white fill-current dark:text-black" />
+           <img src="{{ asset('images/logo.png') }}" alt="Logo">
         </div>
     </div>
 </div>
