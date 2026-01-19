56594ff309b306c7c69cc1bbe6331f210e930291
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-19 11:36:25 +0800

    sas

--- resources/views/components/form/searchable-dropdown.blade.php
+++ resources/views/components/form/searchable-dropdown.blade.php
@@ -6,9 +6,9 @@
     'options' => [],          // Data array/collection hasil search
     'showdropdown' => false,  // Boolean untuk kontrol visibility dropdown
     'required' => false,
-    'disabled' => false,
+    'disabled' => false, // Tambahkan prop disabled
     'clickaction' => 'selectLocation',
-    'namedb' => 'name'
+    'namedb' => 'name' // Nama fungsi di Parent
 ])
 
 <fieldset class="fieldset">
@@ -16,48 +16,37 @@
         <x-form.label :label="$label" :required="$required" />
     @endif
 
-    {{-- PERBAIKAN: Menggunakan @entangle langsung ke properti showdropdown --}}
-    {{-- Menambahkan @click.away agar dropdown tertutup saat klik di luar --}}
-    <div class="relative"
-         x-data="{ open: @entangle($showdropdown) }"
-         @click.away="open = false">
-
+    <div class="relative" x-data="{ open: @entangle($attributes->wire('model').'.live') }">
         <input
-            {{ $disabled ? 'disabled' : '' }}
+        {{ $disabled ? 'disabled' : '' }}
             type="text"
             wire:model.live.debounce.300ms="{{ $modelsearch }}"
             placeholder="{{ $placeholder }}"
-            {{-- PERBAIKAN: Memastikan 'open' menjadi true saat input difokuskan --}}
-            @focus="open = true"
             {{ $attributes->merge([
                 'class' => 'input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs ' .
-                ($modelid && $errors->has($modelid) ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '')
+                ($errors->has($modelid) ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '')
             ]) }}
         />
 
-        {{-- PERBAIKAN: Menggunakan x-show agar AlpineJS bisa mengontrol transisi --}}
-        {{-- Tetap mengecek isi options agar tidak muncul kotak kosong --}}
-        <ul x-show="open && @js(count($options) > 0)"
-            x-cloak
-            class="absolute z-50 w-full mt-1 overflow-auto border rounded-md shadow bg-base-100 max-h-60">
+        @if ($showdropdown && count($options) > 0)
+            <ul class="absolute z-50 w-full mt-1 overflow-auto border rounded-md shadow bg-base-100 max-h-60">
 
-            {{-- Spinner Loading (Target ke modelsearch agar muncul saat mengetik) --}}
-            <div wire:loading wire:target="{{ $modelsearch }}" class="p-2 text-center">
-                <span class="loading loading-spinner loading-sm text-secondary"></span>
-            </div>
+                {{-- Spinner Loading --}}
+                <div wire:loading wire:target="{{ $clickaction }}" class="p-2 text-center">
+                    <span class="loading loading-spinner loading-sm text-secondary"></span>
+                </div>
 
-            @foreach ($options as $opt)
-                <li
-                    {{-- PERBAIKAN: Menambahkan .prevent dan memastikan dropdown tertutup setelah pilih --}}
-                    wire:click.prevent="{{ $clickaction }}({{ $opt->id }}, '{{ addslashes($opt->{$namedb}) }}')"
-                    x-on:click="open = false"
-                    wire:key="opt-{{ $opt->id }}"
-                    class="px-3 py-2 text-sm cursor-pointer hover:bg-base-200"
-                >
-                    {{ $opt->{$namedb} }}
-                </li>
-            @endforeach
-        </ul>
+                @foreach ($options as $opt)
+                    <li
+                        wire:click="{{ $clickaction }}({{ $opt->id }}, '{{ addslashes($opt->{$namedb}) }}')"
+                        wire:key="opt-{{ $opt->id }}"
+                        class="px-3 py-2 text-sm cursor-pointer hover:bg-base-200"
+                    >
+                        {{ $opt->{$namedb} }}
+                    </li>
+                @endforeach
+            </ul>
+        @endif
     </div>
 
     @if($modelid)
