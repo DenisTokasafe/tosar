e0aa3408deeb6a6f995d42b4628c45a072563ff1
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-22 09:04:19 +0800

    sadasd

--- database/migrations/2026_01_22_090149_add_review_date_in_wpi_reports_table.php
+++ database/migrations/2026_01_22_090149_add_review_date_in_wpi_reports_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('wpi_reports', function (Blueprint $table) {
-            $table->date('report_date')->after('review_id')->nullable();
+            $table->date('review_date')->after('review_id')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('wpi_reports', function (Blueprint $table) {
-            $table->date('report_date')->after('review_id')->nullable();
+            $table->date('review_date')->after('review_id')->nullable();
         });
     }
 };
