ff2dfd8b2ab1af5a5634b519ba189d8c11424221
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-26 09:55:05 +0800

    sasfddf

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -21,16 +21,27 @@
                 <h3 class="mb-3 font-bold">Kondisi Checklist ({{ $type }}):</h3>
                 <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
                     @foreach ($fields[$type] as $field)
-                        <fieldset class="flex items-center space-x-6 font-sans text-xs">
-                           <div class="text-black">{{ $field }}</div>
-                            <label class="label">
-                                Yes
-                                <input type="radio"  class="radio radio-xs" wire:model="conditions.{{ $field }}"
-                                    value="yes" />
-                                No
-                                <input type="radio" class="radio radio-xs" wire:model="conditions.{{ $field }}"
-                                    value="no" />
-                            </label>
+                        <fieldset
+                            class="flex items-center p-2 font-sans text-xs bg-white border border-gray-100 rounded shadow-sm">
+                            <div class="text-black font-medium min-w-[80px]">
+                                {{ $field }}
+                            </div>
+
+                            <div class="h-5 w-[1px] bg-gray-300 mx-4"></div>
+
+                            <div class="flex items-center space-x-4">
+                                <label class="flex items-center space-x-2 cursor-pointer group">
+                                    <span class="text-gray-600">yes</span>
+                                    <input type="radio" wire:model="conditions.{{ $field }}" value="yes"
+                                        class="shadow-sm radio radio-primary radio-xs" />
+                                </label>
+
+                                <label class="flex items-center space-x-2 cursor-pointer group">
+                                    <span class="text-gray-600">No</span>
+                                    <input type="radio" wire:model="conditions.{{ $field }}" value="no"
+                                        class="shadow-sm radio radio-primary radio-xs" />
+                                </label>
+                            </div>
                         </fieldset>
                     @endforeach
                 </div>
