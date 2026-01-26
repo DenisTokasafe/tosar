0e337bd27aa686113dc52289dfe4463681be687a
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-26 12:17:38 +0800

    dssas

--- app/Livewire/Inspection/FireInspection.php
+++ app/Livewire/Inspection/FireInspection.php
@@ -3,14 +3,42 @@
 namespace App\Livewire\Inspection;
 
 use Livewire\Component;
+use App\Helpers\FileHelper;
+use Livewire\Attributes\Validate;
 
 class FireInspection extends Component
 {
     public $type = 'Fire Extinguisher'; // Default
-    public $location, $inspection_date, $inspected_by, $remarks;
-
+    public $location, $inspection_date, $inspected_by, $remarks,$area;
+    #[Validate('nullable|file|mimes:jpg,jpeg,png,pdf,doc,docx')]
+    public $dokumentasi;
     // Tempat menyimpan hasil checklist
     public $conditions = [];
+
+    public function rules()
+    {
+        return [
+            'location'        => 'required|string|max:255',
+            'inspection_date' => 'required|date',
+            'inspected_by'    => 'required|string|max:255',
+            'conditions'      => 'required|array',
+            'type'            => 'required|string',
+            'area'            => 'required|string|max:255',
+            'dokumentasi'  => 'nullable|file|mimes:jpg,jpeg,png,pdf,doc,docx|max:2048',
+        ];
+    }
+
+    protected $messages = [
+        'location.required'        => 'Lokasi inspeksi wajib diisi.',
+        'inspection_date.required' => 'Tanggal inspeksi wajib diisi.',
+        'inspected_by.required'    => 'Nama pemeriksa wajib diisi.',
+        'conditions.required'     => 'Checklist kondisi alat wajib diisi.',
+        'type.required'           => 'Jenis alat wajib diisi.',
+        'area.required'           => 'Area wajib diisi.',
+        'dokumentasi.file'  => 'File dokumentasi harus berupa berkas yang valid.',
+        'dokumentasi.mimes' => 'File dokumentasi hanya boleh dalam format JPG, JPEG, PNG, DOC, DOCX atau PDF.',
+        'dokumentasi.max'   => 'Ukuran file dokumentasi maksimal 2 MB.',
+    ];
 
     // Definisi kriteria berdasarkan gambar yang Anda berikan
     public $fields = [
@@ -18,7 +46,7 @@ class FireInspection extends Component
         'Eye Wash & Safety Shower' => ['Air', 'Penutup', 'Nozzle', 'Handle', 'Sign', 'Access', 'Kebersihan'],
         'Fire Hose' => ['Hose', 'Reel', 'Nozzle', 'Valve', 'Air', 'Cover'],
         'Fire Hydrant' => ['Air', 'Kaca', 'Nozzle', 'Box', 'Hose', 'Kunci Hydrant'],
-        'Fire sprinkler system' => ['Line Pipa', 'Main Valve', 'Drain Valve', 'Test valve', 'Alarm', 'Pressure','Access'],
+        'Fire sprinkler system' => ['Line Pipa', 'Main Valve', 'Drain Valve', 'Test valve', 'Alarm', 'Pressure', 'Access'],
         'Ring Buoy' => ['Ring Buoy', 'Access', 'Tempat Ring Buoy', 'Tali'],
         'Muster Point' => ['Access', 'Visibility', 'Colour', 'Condition of Board', 'Condition of Pole', 'Letter'],
     ];
@@ -34,17 +62,39 @@ class FireInspection extends Component
 
     public function save()
     {
+         if ($this->dokumentasi) {
+                $docCorrectivePath = FileHelper::compressAndStore($this->dokumentasi, 'inspections/documents');
+            }
         FireInspection::create([
             'type' => $this->type,
             'location' => $this->location,
+            'area' => $this->area,
+            'dokumentasi' => $docCorrectivePath,
             'inspection_date' => $this->inspection_date,
             'inspected_by' => $this->inspected_by,
             'conditions' => $this->conditions, // Menyimpan array sebagai JSON
             'remarks' => $this->remarks,
         ]);
 
-        $this->reset(['conditions', 'remarks', 'location']);
-        session()->flash('message', 'Inspeksi berhasil dicatat.');
+        $this->resetForm();
+         $this->dispatch('alert', [
+            'text' => "Data Inspeksi berhasil disimpan!",
+            'duration' => 5000,
+            'destination' => '/contact',
+            'newWindow' => true,
+            'close' => true,
+            'backgroundColor' => "background: linear-gradient(135deg, #00c853, #00bfa5);",
+        ]);
+    }
+    public function resetForm()
+    {
+        $this->location = null;
+        $this->inspection_date = null;
+        $this->inspected_by = null;
+        $this->remarks = null;
+        $this->area = null;
+        $this->dokumentasi = null;
+        $this->conditions = [];
     }
     public function render()
     {
--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -19,7 +19,7 @@
             <div class="grid grid-cols-1 gap-2 mb-4 md:grid-cols-3">
                 <x-form.input-floating label="Area" wire:model="area" required />
                 <x-form.input-floating label="Lokasi" wire:model="location" required />
-                <x-form.datepicker label="Tanggal / Date" model="inspection_date"  />
+                <x-form.datepicker label="Tanggal / Date" model="inspection_date" />
             </div>
 
             <div class="p-4 border rounded-lg bg-gray-50">
@@ -39,6 +39,43 @@
             </div>
 
             <textarea wire:model="remarks" placeholder="Remarks/Catatan..." class="w-full p-2 mt-4 border rounded"></textarea>
+            <fieldset class=" fieldset">
+                <x-form.upload label="Lampirkan foto atau dokumentasi" model="dokumentasi" :file="$dokumentasi" />
+                <div wire:loading.remove wire:target="dokumentasi">
+                    @if ($dokumentasi)
+                        @if (in_array($dokumentasi->getClientOriginalExtension(), ['jpg', 'jpeg', 'png']))
+                            <img src="{{ $dokumentasi->temporaryUrl() }}"
+                                class="mt-2 {{ $dokumentasi ? 'w-40' : '' }} h-auto rounded border" />
+                        @elseif (in_array($dokumentasi->getClientOriginalExtension(), ['pdf', 'doc', 'docx']))
+                            <div class="flex items-center gap-2 mt-2">
+                                @if ($dokumentasi->getClientOriginalExtension() == 'pdf')
+                                    <x-icon.pdf class="w-8 h-8" />
+                                    <span
+                                        class="text-sm text-red-600">{{ $dokumentasi->getClientOriginalName() }}</span>
+                                @elseif (in_array($dokumentasi->getClientOriginalExtension(), ['doc', 'docx']))
+                                    <x-icon.word class="w-8 h-8" />
+                                    <span
+                                        class="text-sm text-blue-600">{{ $dokumentasi->getClientOriginalName() }}</span>
+                                @else
+                                    {{-- Ikon generik untuk file lain --}}
+                                    <svg class="w-8 h-8 text-gray-400" fill="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zM6 20V4h7v4h4v12H6z" />
+                                    </svg>
+                                    <span class="text-sm text-gray-600">File:
+                                        {{ $dokumentasi->getClientOriginalName() }}</span>
+                                @endif
+                            </div>
+                        @else
+                            <p class="mt-2 text-sm text-gray-600">File:
+                                {{ $dokumentasi->getClientOriginalName() }}
+                            </p>
+                        @endif
+                    @endif
+                </div>
+                <x-label-error :messages="$errors->get('dokumentasi')" />
+            </fieldset>
 
             <button wire:click="save"
                 class="w-full py-2 mt-4 font-bold text-white bg-blue-600 rounded hover:bg-blue-700">
