4b18ec1f3a3eb149fe13b2acbf56c1ba94f9d32a
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-26 09:33:21 +0800

    asdf

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -21,25 +21,17 @@
                 <h3 class="mb-3 font-bold">Kondisi Checklist ({{ $type }}):</h3>
                 <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
                     @foreach ($fields[$type] as $field)
-                        <div class="flex items-center justify-between p-2 bg-white border rounded shadow-sm">
-                            <span class="text-sm font-medium text-gray-700">{{ $field }}</span>
-                            <div class="flex gap-2">
-                                <label class="flex items-center cursor-pointer">
-                                    <input type="radio" wire:model="conditions.{{ $field }}" value="Good"
-                                        class="hidden peer">
-                                    <span
-                                        class="px-2 py-1 text-xs border rounded peer-checked:bg-green-500 peer-checked:text-white">✔
-                                        Good</span>
-                                </label>
-                                <label class="flex items-center cursor-pointer">
-                                    <input type="radio" wire:model="conditions.{{ $field }}" value="No Good"
-                                        class="hidden peer">
-                                    <span
-                                        class="px-2 py-1 text-xs border rounded peer-checked:bg-red-500 peer-checked:text-white">✘
-                                        No Good</span>
-                                </label>
-                            </div>
-                        </div>
+                        <fieldset class="w-64 p-4 border fieldset">
+                            <legend class="fieldset-legend">{{ $field }}</legend>
+                            <label class="label">
+                                Yes
+                                <input type="checkbox" checked="checked" wire:model="conditions.{{ $field }}"
+                                    value="yes" />
+                                No
+                                <input type="checkbox" class="checkbox" wire:model="conditions.{{ $field }}"
+                                    value="no" />
+                            </label>
+                        </fieldset>
                     @endforeach
                 </div>
             </div>
