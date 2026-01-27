ffbb1e26d0bf7281b12871638eec12a25f61d6a2
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-27 09:15:00 +0800

    sdf

--- resources/views/components/tabs-wpi/layout.blade.php
+++ resources/views/components/tabs-wpi/layout.blade.php
@@ -7,7 +7,7 @@
                 </div>
                 <div class="flex items-center gap-3">
                     @if (Route::is('wpi.list'))
-                        <a href="{{ route('wpi.create') }}" class="text-xs uppercase btn btn-primary btn-xs">
+                        <a href="{{ route('wpi.create') }}" class="text-xs uppercase btn btn-primary btn-xs btn-soft">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus">
@@ -17,7 +17,7 @@
                         </a>
                     @endif
                     @if (Route::is('fire-inspection-list'))
-                        <a href="{{ route('fire-inspection') }}" class="text-xs uppercase btn btn-primary btn-xs">
+                        <a href="{{ route('fire-inspection') }}" class="text-xs uppercase btn btn-primary btn-xs btn-soft">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus">
