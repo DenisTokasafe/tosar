5e7f9ce461159248cf256688f466d2afd32d6509
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-27 08:55:23 +0800

    dsdsd

--- app/Livewire/Inspection/FireInspection.php
+++ app/Livewire/Inspection/FireInspection.php
@@ -46,7 +46,6 @@ class FireInspection extends Component
             'inspected_users' => 'required|array|min:1', // Menggunakan array user dari jawaban sebelumnya
             'conditions'      => 'required|array',
             'type'            => 'required|string',
-            'area'            => 'required|string|max:255',
             'searchLocation' => 'required|string',
             'dokumentasi'     => 'nullable|file|mimes:jpg,jpeg,png,pdf,doc,docx|max:2048',
         ];
@@ -76,7 +75,6 @@ class FireInspection extends Component
             'conditions.required'      => 'Checklist kondisi alat wajib diisi.',
             'conditions.*.required'    => 'Semua poin checklist wajib diisi/pilih.', // Pesan untuk item dinamis
             'type.required'            => 'Jenis alat wajib diisi.',
-            'area.required'            => 'Area wajib diisi.',
             'searchLocation.required'  => 'Area wajib diisi.',
             'dokumentasi.max'          => 'Ukuran file dokumentasi maksimal 2 MB.',
             // ... sisa pesan Anda
