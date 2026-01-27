5115220f828cbfe060b44a78a27899e88fdbbc59
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-27 09:13:01 +0800

    sas

--- resources/views/components/tabs-wpi/layout.blade.php
+++ resources/views/components/tabs-wpi/layout.blade.php
@@ -16,6 +16,16 @@
                             </svg> Laporan Baru
                         </a>
                     @endif
+                    @if (Route::is('fire-inspection-list'))
+                        <a href="{{ route('fire-inspection') }}" class="text-xs uppercase btn btn-primary btn-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus">
+                                <path d="M5 12h14" />
+                                <path d="M12 5v14" />
+                            </svg> Laporan Baru
+                        </a>
+                    @endif
                 </div>
             </div>
             <div
