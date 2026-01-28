8d9705914f5bd461154d306234cc069182f28091
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-28 08:25:37 +0800

    asdfa

--- resources/views/livewire/inspection/fire-inspection-edit.blade.php
+++ resources/views/livewire/inspection/fire-inspection-edit.blade.php
@@ -152,7 +152,7 @@
 
             <div class="flex justify-end pt-4 mt-6 border-t">
                 <button wire:click="save" wire:loading.attr="disabled" class="btn btn-soft btn-success btn-sm">
-                    <span wire:loading wire:target="save" class="loading loading-spinner loading-xs"></span>
+                    <span wire:loading.remove.class='hidden' class="hidden" wire:target="save" class="loading loading-spinner loading-xs"></span>
                     Simpan Laporan
                 </button>
             </div>
