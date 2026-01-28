c859c232d773e520a7c99e460910d90ba99c6f8a
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-28 11:23:42 +0800

    dasdasdasd

--- database/migrations/2026_01_26_081658_create_fire_protections_table.php
+++ database/migrations/2026_01_26_081658_create_fire_protections_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
     {
         Schema::create('fire_protections', function (Blueprint $table) {
             $table->id();
-            $table->string('type'); // Contoh: 'Fire Extinguisher', 'Hydrant', 'Eye Wash'
-            $table->string('location');
-            $table->string('equipment_no')->nullable();
             $table->date('inspection_date');
             $table->json('inspected_by')->nullable();
             // Menggunakan JSON untuk menyimpan checklist kondisi agar fleksibel
