fa8513fe369b4d464beafb273925da5807c1d810
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-28 17:37:58 +0800

    fddff

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -53,7 +53,6 @@
                             ? array_keys($firstEquipment->technical_data)
                             : [];
                 @endphp
-{{ $firstEquipment }}
                 <table class="table w-full border-collapse table-xs">
                     <thead>
                         <tr class="text-white bg-slate-700">
