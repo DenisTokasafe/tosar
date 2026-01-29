6342b5caf3f1324c577195fad80491d14912498c
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-29 09:38:10 +0800

    asdasd

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -53,7 +53,7 @@
                             ? array_keys($firstEquipment->technical_data)
                             : [];
                 @endphp
-                <div class="overflow-x-auto max-h-[calc(100vh-20rem)] w-dvw">
+                <div class="overflow-x-auto max-h-[calc(100vh-20rem)] ">
                     <table class="table text-xs border-collapse table-xs table-pin-rows">
                         <thead>
                             <tr class="text-center ">
