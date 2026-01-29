93abc17e7e7b6bbca3c5f6dd68153b0c1faddc2c
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-01-29 08:23:32 +0800

    kjjkjkjk

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -53,7 +53,7 @@
                             ? array_keys($firstEquipment->technical_data)
                             : [];
                 @endphp
-                <div class="w-full overflow-x-auto max-w-9/10">
+                <div class="w-full overflow-x-auto max-w-1/2">
                     <table class="table text-xs border-collapse table-xs">
                         <thead>
                             <tr class="text-white bg-slate-700">
