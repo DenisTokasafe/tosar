910cb29e1546f28b62f0cdc845c933aaa5d2b9e6
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-02 14:25:02 +0800

    hgh

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -10,28 +10,30 @@
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
     <!-- Validation Errors -->
-        <div class="w-full join">
-            <div>
-                <label class="w-full input input-bordered focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs validator join-item">
-                    <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
-                            stroke="currentColor">
-                            <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                        </g>
-                    </svg>
-                    <input wire:model.live="check_id" type="text" placeholder="input nomor id anda" required />
-                </label>
-                <div class="hidden validator-hint">Enter valid email address</div>
-            </div>
-            <button wire:click='checkId' class="btn btn-xs btn-neutral join-item">Check ID</button>
+    <div class="w-full join">
+        <div>
+            <label
+                class="w-full input input-bordered focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs validator join-item">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-id-card-icon lucide-id-card">
+                    <path d="M16 10h2" />
+                    <path d="M16 14h2" />
+                    <path d="M6.17 15a3 3 0 0 1 5.66 0" />
+                    <circle cx="9" cy="11" r="2" />
+                    <rect x="2" y="5" width="20" height="14" rx="2" />
+                </svg>
+                <input wire:model.live="check_id" type="text" placeholder="input nomor id anda" required />
+            </label>
+            <div class="hidden validator-hint">Enter valid email address</div>
         </div>
-         @if ($check_no_id_status)
-            <p
-                class="text-xs {{ str_contains($check_no_id_status, 'tersedia') ? 'text-green-500' : 'text-red-500' }}">
-                {{ $check_no_id_status }}
-            </p>
-        @endif
+        <button wire:click='checkId' class="btn btn-xs btn-neutral join-item">Check ID</button>
+    </div>
+    @if ($check_no_id_status)
+        <p class="text-xs {{ str_contains($check_no_id_status, 'tersedia') ? 'text-green-500' : 'text-red-500' }}">
+            {{ $check_no_id_status }}
+        </p>
+    @endif
 
     <form wire:submit="register">
 
