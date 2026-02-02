31bea5dbc45646a7c765ef0d96f6c4c53fa67fd2
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-02 14:10:41 +0800

    asdasd

--- resources/views/livewire/manhours/grafik/index.blade.php
+++ resources/views/livewire/manhours/grafik/index.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+<div class="grid grid-cols-1 gap-2 mb-5 lg:grid-cols-2">
     <div wire:ignore id="grafik-manhours" style="height: 320px"></div>
     {{-- Gunakan grafik-manhours untuk Line Chart Gabungan --}}
     <div wire:ignore id="grafik-manpower" style="height: 320px"></div>
