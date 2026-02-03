2230bcdb5ec82640100a1e4e094dd6a2d725a3b0
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-03 11:27:00 +0800

    asas

--- resources/views/components/tabs-wpi/layout.blade.php
+++ resources/views/components/tabs-wpi/layout.blade.php
@@ -29,7 +29,7 @@
                 </div>
             </div>
             <div
-                class="flex w-full flex-1 flex-col gap-4 rounded-xl inset-shadow-sm h-full max-h-[calc(100vh-16rem)] sm:max-h-[calc(100vh-16rem)] md:max-h-[calc(100vh-14rem)] lg:max-h-[calc(100vh-14rem)] 2xl:max-h-[calc(100vh-14rem)] relative">
+                class="flex  flex-1 flex-col gap-4 rounded-xl inset-shadow-sm h-full max-h-[calc(100vh-16rem)] sm:max-h-[calc(100vh-16rem)] md:max-h-[calc(100vh-14rem)] lg:max-h-[calc(100vh-14rem)] 2xl:max-h-[calc(100vh-14rem)] relative">
                 <div
                     class="flex-1 h-full p-4 overflow-x-hidden overflow-y-auto border rounded-xl border-neutral-200 dark:border-base-200">
                     <div class="w-full max-w-full ">
