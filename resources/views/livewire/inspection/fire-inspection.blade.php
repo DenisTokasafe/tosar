7d6462de9bff859ea063862e807d979857ee1464
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-03 12:37:17 +0800

    sasas

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -11,7 +11,7 @@
     <x-tabs-wpi.layout>
         <div class="p-6 bg-white ">
             <div class="flex md:justify-start ">
-                <div class="flex items-center w-full max-w-md gap-4">
+                <div class="flex items-center w-full max-w-lg gap-4">
 
                     <select wire:model.live="type"
                         class="select select-xs select-bordered w-full focus-within:outline-none focus-within:border-info focus-within:ring-0 {{ $errors->has('type') ? 'border-rose-500' : '' }}">
