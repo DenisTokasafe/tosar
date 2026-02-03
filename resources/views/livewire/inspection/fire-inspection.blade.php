920aae4c14f12965a218a1858e42513efea39af7
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-03 11:41:40 +0800

    asas

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -43,7 +43,7 @@
                         : [];
             @endphp
 
-            <div class="overflow-x-auto max-h-[calc(100vh-25rem)] 2xl:max-h-[calc(100vh-37rem)] max-w-[calc(100vh-25rem)] border rounded-lg ">
+            <div class="overflow-x-auto max-h-[calc(100vh-25rem)] 2xl:max-h-[calc(100vh-37rem)] max-w-[calc(100vh-80rem)] border rounded-lg ">
                 <table class="table border-separate table-xs table-pin-rows border-spacing-0">
                     <thead>
                         <tr class="capitalize bg-slate-100 text-slate-700">
