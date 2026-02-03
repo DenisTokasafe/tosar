b2e936c46d0849b6df77a93d803ef5335eab0243
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-03 11:59:31 +0800

    dsd

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -70,7 +70,7 @@
                     <tbody>
                         @forelse ($allMasterData as $master)
                             <tr class="transition-colors hover:bg-blue-50/30">
-                                <td class="sticky left-0 z-10 font-medium bg-white border-b border-r">
+                                <td class="font-medium bg-white border-b border-r ">
                                     {{ $master->specific_location }}</td>
 
                                 @foreach ($techKeys as $key)
