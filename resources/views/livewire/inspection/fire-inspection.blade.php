146415cba7cbd9101350ab1aebce23925f816acd
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-04 09:19:33 +0800

    sd

--- resources/views/livewire/inspection/fire-inspection.blade.php
+++ resources/views/livewire/inspection/fire-inspection.blade.php
@@ -55,7 +55,7 @@
             @endphp
 
             <div class="overflow-x-auto max-h-[calc(100vh-25rem)] 2xl:max-h-[calc(100vh-37rem)]  border rounded-lg ">
-                @if ($firstEquipment->location_id)
+                @if (!empty($firstEquipment->location_id))
                     <table class="table border-separate md:table-fixed table-xs table-pin-rows border-spacing-0">
                         <thead>
                             <tr class="capitalize bg-slate-100 text-slate-700">
