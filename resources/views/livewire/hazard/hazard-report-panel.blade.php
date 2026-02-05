2b1fd966cecf5bb31cedab0584d17cfa6daa173e
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-05 08:08:38 +0800

    iik

--- resources/views/livewire/hazard/hazard-report-panel.blade.php
+++ resources/views/livewire/hazard/hazard-report-panel.blade.php
@@ -74,7 +74,7 @@
         </div>
     </div>
       {{-- <x-manhours.layout> --}}
-    <div class="mt-4 overflow-x-auto max-h-[calc(100vh-14rem)] 2xl:max-h-[calc(100vh-16rem)] ">
+    <div class="mt-4 overflow-x-auto max-h-[calc(100vh-16rem)] 2xl:max-h-[calc(100vh-20rem)] ">
         <table class="table text-xs border table-xs">
             <thead>
                 <tr class="bg-gray-100">
