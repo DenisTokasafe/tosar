9d9e07a0257e0d29d58dd665f97d9edb5aededd3
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-11 15:34:44 +0800

    ds

--- resources/views/livewire/manhours/index.blade.php
+++ resources/views/livewire/manhours/index.blade.php
@@ -327,7 +327,7 @@
 
                         {{-- Tombol Aksi --}}
                         <div class="flex justify-end gap-2 mt-2">
-                            <flux:button size="xs" variant="danger" wire:click="close_modal">Batal</flux:button>
+                            <flux:button size="xs" variant="danger" onclick="manhours_modal.close()">Batal</flux:button>
                             @if ($selectedId)
                                 <flux:button size="xs" variant="primary" type="submit">Update</flux:button>
                             @else
