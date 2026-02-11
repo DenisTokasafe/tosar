ba8407f3869dc00eeeb2699379f0dbab27390882
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-11 08:56:54 +0800

    dsl

--- resources/views/livewire/administration/equipment-master/index.blade.php
+++ resources/views/livewire/administration/equipment-master/index.blade.php
@@ -225,10 +225,10 @@
                                     </td>
                                     <td class="flex gap-1">
                                         <button wire:click="edit({{ $item->id }})"
-                                            class="btn btn-xs btn-info">Edit</button>
+                                            class="btn btn-xs btn-square btn-soft btn-info"> <x-icon.edit /> </button>
                                         <button onclick="confirm('Hapus?') || event.stopImmediatePropagation()"
                                             wire:click="delete({{ $item->id }})"
-                                            class="btn btn-xs btn-error">Hapus</button>
+                                            class="btn btn-xs btn-square btn-soft btn-error"><x-icon.delete /></button>
                                     </td>
                                 </tr>
                             @empty
