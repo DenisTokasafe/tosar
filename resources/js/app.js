ef8fd964089301855cec842ee7ff70dd670d43e9
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-18 14:54:20 +0800

    sdfg

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,13 @@ import "toastify-js/src/toastify.css";
 import Toastify from "toastify-js";
 window.Toastify = Toastify
 import flatpickr from "flatpickr";
+import monthSelectPlugin from "flatpickr/dist/plugins/monthSelect/index";
 import "flatpickr/dist/flatpickr.min.css";
+import "flatpickr/dist/plugins/monthSelect/style.css";
 import * as echarts from 'echarts';
 window.echarts = echarts
-
+window.flatpickr = flatpickr;
+window.monthSelectPlugin = monthSelectPlugin;
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function (registration) {
     }).catch(function (registrationError) {
