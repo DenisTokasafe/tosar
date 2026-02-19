aa6c174c1b1f618697c13b53748384713d06af17
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-19 14:19:33 +0800

    dasdasd

--- resources/views/components/tabs-people/layout.blade.php
+++ resources/views/components/tabs-people/layout.blade.php
@@ -3,7 +3,7 @@
     <div
         class="flex w-full flex-1 flex-col gap-4   h-full max-h-[calc(100vh-16rem)] sm:max-h-[calc(100vh-16rem)] md:max-h-[calc(100vh-14rem)] lg:max-h-[calc(100vh-14rem)] 2xl:max-h-[calc(100vh-14rem)]">
         <div
-            class="flex-1 h-full p-4 overflow-x-hidden overflow-y-auto border rounded-xl border-neutral-200 dark:border-base-200">
+            class="flex-1 h-full p-4 overflow-x-hidden overflow-y-auto ">
             <div class="w-full max-w-full ">
                 <div class="flex items-start max-md:flex-col">
                     <div class="me-10 w-full pb-4 md:w-[220px]">
