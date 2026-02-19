06ca655b51aad296220454d2238f3d8b827c9daa
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-19 14:16:01 +0800

    asdf

--- resources/views/components/tabs-people/layout.blade.php
+++ resources/views/components/tabs-people/layout.blade.php
@@ -1,8 +1,7 @@
 @props(['idUser'])
 <x-tabs-relation.layout>
-<div class="py-2 ">
     <div
-        class="flex w-full flex-1 flex-col gap-4 rounded-xl inset-shadow-sm h-full max-h-[calc(100vh-16rem)] sm:max-h-[calc(100vh-16rem)] md:max-h-[calc(100vh-14rem)] lg:max-h-[calc(100vh-14rem)] 2xl:max-h-[calc(100vh-14rem)]">
+        class="flex w-full flex-1 flex-col gap-4   h-full max-h-[calc(100vh-16rem)] sm:max-h-[calc(100vh-16rem)] md:max-h-[calc(100vh-14rem)] lg:max-h-[calc(100vh-14rem)] 2xl:max-h-[calc(100vh-14rem)]">
         <div
             class="flex-1 h-full p-4 overflow-x-hidden overflow-y-auto border rounded-xl border-neutral-200 dark:border-base-200">
             <div class="w-full max-w-full ">
@@ -28,5 +27,4 @@
             </div>
         </div>
     </div>
-</div>
 </x-tabs-relation.layout>
