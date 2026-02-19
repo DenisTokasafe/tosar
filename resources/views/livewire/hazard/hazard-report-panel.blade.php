b868ec71d233e48ff42ca81e96e1fe1079a00beb
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-19 08:26:55 +0800

    asdf

--- resources/views/livewire/hazard/hazard-report-panel.blade.php
+++ resources/views/livewire/hazard/hazard-report-panel.blade.php
@@ -23,17 +23,8 @@
                     !empty($filterReporterDept) ||
                     $searchPelapor ||
                     ($start_date && $end_date))
-                <div class="tooltip tooltip-right  mb-0.5" data-tip="Export Excel">
-                    <a href="#" wire:click="export" class="btn btn-square btn-success btn-xs">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="lucide lucide-download-icon lucide-download">
-                            <path d="M12 15V3" />
-                            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                            <path d="m7 10 5 5 5-5" />
-                        </svg>
-                    </a>
-                </div>
+                    <x-button.btn-tooltip color="success" icon="file-donwload" wireClick="export" tooltip="Download" />
+
             @endif
             {{-- @livewire('hazard.import-hazard-reports-modal') --}}
             {{-- Tambahkan wire:model.live untuk memfilter secara real-time --}}
