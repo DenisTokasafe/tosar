7f1a405e7f4e7725c3b7aa84640a7217585e17ee
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-19 14:48:38 +0800

    gddfdff

--- resources/views/components/tabs-people/layout.blade.php
+++ resources/views/components/tabs-people/layout.blade.php
@@ -2,18 +2,20 @@
 <x-tabs-relation.layout>
     <div
         class="flex w-full flex-1 flex-col    h-full max-h-[calc(100vh-16rem)] sm:max-h-[calc(100vh-16rem)] md:max-h-[calc(100vh-14rem)] lg:max-h-[calc(100vh-14rem)] 2xl:max-h-[calc(100vh-14rem)]">
-        <div
-            class="flex-1 h-full p-4 overflow-x-hidden ">
+        <div class="flex-1 h-full p-4 overflow-x-hidden ">
             <div class="w-full max-w-full ">
                 <div class="flex items-start max-md:flex-col">
                     <div class=" gap-4 w-full p-2 md:w-[220px] overflow-y-auto ">
                         <flux:navlist>
-                            <flux:navlist.item :href="route('people.details', $idUser)" wire:navigate>{{ __('Details') }}</flux:navlist.item>
+                            <flux:navlist.item :href="route('people.details', $idUser)" wire:navigate>
+                                {{ __('Details') }}</flux:navlist.item>
                         </flux:navlist>
                     </div>
 
-                    <flux:separator class="md:hidden" />
-                    <flux:separator vertical class="hidden md:block" />
+                    <div class="m-2">
+                        <flux:separator class="md:hidden" />
+                        <flux:separator vertical class="hidden md:block" />
+                    </div>
 
                     <div class="self-stretch flex-1 max-md:pt-6">
                         <flux:heading>{{ $heading ?? '' }}</flux:heading>
