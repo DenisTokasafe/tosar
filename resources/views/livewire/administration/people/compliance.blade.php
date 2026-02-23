d66277ace757a35e1c3ccfea99e3036e98f785c6
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-23 08:38:01 +0800

    sdfsd

--- resources/views/livewire/administration/people/compliance.blade.php
+++ resources/views/livewire/administration/people/compliance.blade.php
@@ -2,11 +2,11 @@
     <x-toast />
     <x-tabs-people.layout :idUser="$userId">
         <div class="flex flex-col items-center p-4 bg-gray-100 md:flex-row md:justify-between">
-            <div class="text-white bg-blue-500 rounded ">
+            <div class="rounded ">
                 Logo/Brand
             </div>
 
-            <div class="text-white bg-green-500 rounded ">
+            <div class="rounded ">
                 <x-button.btn-tooltip color="primary" icon="add" modalId="create_modal" tooltip="Tambah Employee" />
                 <x-button.btn-tooltip modalId="import_modal" color="accent" icon="file-import" tooltip="Import Data" />
             </div>
