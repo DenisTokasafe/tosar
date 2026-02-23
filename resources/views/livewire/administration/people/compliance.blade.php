b88513929a2e9061c17941d0b9ff1797ec469cc7
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-23 14:03:11 +0800

    asas

--- resources/views/livewire/administration/people/compliance.blade.php
+++ resources/views/livewire/administration/people/compliance.blade.php
@@ -2,17 +2,17 @@
     <x-toast />
     <x-tabs-people.layout :idUser="$userId">
 
-            <div class="flex flex-col items-center p-2 mb-5 border rounded-t-lg md:flex-row md:justify-between border-neutral-200 ">
-                <div class="rounded ">
-                    Logo/Brand
-                </div>
-                <div class="flex rounded">
-                    <x-button.btn-tooltip color="primary" icon="add" modalId="create_modal" tooltip="Tambah Employee" />
-                    @livewire('administration.people.compliance-import')
-                </div>
+        <div class="flex flex-col items-center p-2 mb-5 border rounded-t-lg md:flex-row md:justify-between border-neutral-200 ">
+            <div class="rounded ">
+                Logo/Brand
             </div>
+            <div class="flex rounded">
+                <x-button.btn-tooltip color="primary" icon="add" modalId="create_modal" tooltip="Tambah Employee" />
+                @livewire('administration.people.compliance-import')
+            </div>
+        </div>
 
-        <div class="overflow-x-auto">
+        <div class="overflow-x-auto border">
             <table class="table table-xs">
                 <thead>
                     <tr>
