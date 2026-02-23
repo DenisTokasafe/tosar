d774fe00248ab4e073b54e64a204a8dccc89ef05
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-23 09:58:07 +0800

    sas

--- resources/views/livewire/administration/people/compliance-import.blade.php
+++ resources/views/livewire/administration/people/compliance-import.blade.php
@@ -28,11 +28,11 @@
                         <button class="btn btn-soft btn-error btn-xs" wire:click="closeModal">Close</button>
                     </form>
                     {{-- Tombol Submit --}}
-                    <button class="btn btn-xs btn-soft btn-success" wire:loading.attr="disabled" wire:click="import">
-                        <span wire:loading.add.class='hidden' wire:target='import'><x-icon.file-import /></span>
-                        <span wire:loading.remove.class='hidden' wire:target='import' class="hidden loading loading-bars loading-xs"></span>
-                        <span wire:loading.add.class='hidden' wire:target='import'>Upload</span>
-                        <span wire:loading.remove.class='hidden' wire:target='import' class="hidden">Proses Upload</span>
+                    <button class="btn btn-xs btn-soft btn-success" wire:loading.attr="disabled" wire:click="import,file">
+                        <span wire:loading.add.class='hidden' wire:target='import,file'><x-icon.file-import /></span>
+                        <span wire:loading.remove.class='hidden' wire:target='import,file' class="hidden loading loading-bars loading-xs"></span>
+                        <span wire:loading.add.class='hidden' wire:target='import,file'>Upload</span>
+                        <span wire:loading.remove.class='hidden' wire:target='import,file' class="hidden">Proses Upload</span>
                     </button>
                 </div>
 
