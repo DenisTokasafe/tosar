9a64136bf860075b503da52422079bf104705a86
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-23 14:43:11 +0800

    sas

--- resources/views/livewire/administration/people/compliance.blade.php
+++ resources/views/livewire/administration/people/compliance.blade.php
@@ -62,7 +62,7 @@
                         <x-label-error :messages="$errors->get('class')" />
                     </fieldset>
                 <fieldset class="fieldset">
-                        <x-form.label label="Pilih Class" required />
+                        <x-form.label label="Description" required />
                         <select wire:model.live="compliance_name"
                             class="w-full select select-bordered select-xs focus-within:outline-none focus-within:border-info focus-within:ring-0">
                             <option value="">-- Select Existing Compliance --</option>
