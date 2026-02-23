bf7ef0c240afe9fc809f531f036a6e4c802696a0
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-23 08:39:11 +0800

    saasas

--- resources/views/livewire/administration/people/compliance.blade.php
+++ resources/views/livewire/administration/people/compliance.blade.php
@@ -1,7 +1,7 @@
 <section class="w-full">
     <x-toast />
     <x-tabs-people.layout :idUser="$userId">
-        <div class="flex flex-col items-center p-4 bg-gray-100 md:flex-row md:justify-between">
+        <div class="flex flex-col items-center p-4 md:flex-row md:justify-between">
             <div class="rounded ">
                 Logo/Brand
             </div>
