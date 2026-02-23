dcba32223a63176e19205de44e233c3c55b4214c
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-23 08:35:05 +0800

    daasdda

--- resources/views/livewire/administration/people/compliance.blade.php
+++ resources/views/livewire/administration/people/compliance.blade.php
@@ -1,6 +1,14 @@
 <section class="w-full">
     <x-toast />
     <x-tabs-people.layout :idUser="$userId">
-        tes
+        <div class="flex flex-col items-center p-4 bg-gray-100 md:flex-row md:justify-between">
+            <div class="p-4 text-white bg-blue-500 rounded">
+                Logo/Brand
+            </div>
+
+            <div class="p-4 text-white bg-green-500 rounded">
+                Menu Navigasi
+            </div>
+        </div>
     </x-tabs-people.layout>
 </section>
