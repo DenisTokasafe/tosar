c447c75a0888353b62de43469d3cd3f265c41bb4
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-23 13:12:19 +0800

    sa

--- resources/views/livewire/administration/people/compliance.blade.php
+++ resources/views/livewire/administration/people/compliance.blade.php
@@ -1,8 +1,8 @@
 <section class="w-full">
     <x-toast />
     <x-tabs-people.layout :idUser="$userId">
-        <div class="static">
-            <div class="flex flex-col items-center p-2 mb-10 border rounded-t-lg md:flex-row md:justify-between border-neutral-200 md:absolute md:inset-x-0 md:top-0 md:z-20 ">
+
+            <div class="flex flex-col items-center p-2 mb-10 border rounded-t-lg md:flex-row md:justify-between border-neutral-200 md:sticky md:top-0 md:z-20 ">
                 <div class="rounded ">
                     Logo/Brand
                 </div>
@@ -12,7 +12,7 @@
                     @livewire('administration.people.compliance-import')
                 </div>
             </div>
-        </div>
+
         <div class="overflow-x-auto">
             <table class="table table-xs">
                 <thead>
