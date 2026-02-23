68da374ad6bd6f29a995f9cfce5202092590bca0
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-23 08:25:45 +0800

    adsf

--- app/Livewire/Administration/People/Compliance.php
+++ app/Livewire/Administration/People/Compliance.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class Compliance extends Component
 {
+    public $userId;
+    public function mount($id)
+    {
+        $user = User::findOrFail($id);
+
+        // ❗ PINDAHKAN INI KE ATAS: Set $this->userId DULU
+        $this->userId = $user->id;
+    }
     public function render()
     {
         return view('livewire.administration.people.compliance');
