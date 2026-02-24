f150e03d817c1fbe1fa233fb7cdcb6de47c1603d
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-24 15:08:16 +0800

    ads

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,5 +1,5 @@
 <label {{ $attributes->merge(['class' => 'text-xs font-medium capitalize dark:text-slate-500 text-black']) }}>
-    {{ $label }}
+    {{ __($label) }}
     @if ($required)
     <span class="font-bold text-red-500">*</span>
     @endif
