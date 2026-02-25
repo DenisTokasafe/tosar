5cb83af58abdeaaffc14db1b189e86652e73b67c
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-25 13:24:36 +0800

    asd

--- resources/views/livewire/language-switcher.blade.php
+++ resources/views/livewire/language-switcher.blade.php
@@ -2,7 +2,7 @@
 
     <select wire:model.live="lang" name="" id=""class=" select select-xs select-bordered focus-within:outline-none focus-within:border-info focus-within:ring-0">
          <option value="">{{ __('Pilih Bahasa') }}</option>
-         <option value="en"><i class="fi fi-id"></i>{{ __('English') }}</option>
-         <option value="id"><i class="fi fi-gb"></i>{{ __('Bahasa Indonesia') }}</option>
+         <option value="en"><i class="inline-block w-8 h-8 text-2xl fi fi-gb"></i>{{ __('English') }}</option>
+         <option value="id"><i class="inline-block w-8 h-8 text-2xl fi fi-br-indonesia"></i></i>{{ __('Bahasa Indonesia') }}</option>
     </select>
 </div>
