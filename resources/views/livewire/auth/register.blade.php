3974fa6ac39f33235f36174af9102b57c7a86f16
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-25 13:12:35 +0800

    adasas

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -33,7 +33,7 @@
         <p class="text-xs text-green-500">
             {{ $check_no_id_status }}
         </p>
-            <x-auth-session-status class="text-center" :status="session('status')" />
+
         <div class="flex flex-col justify-center gap-4">
             <x-form.input-text label="Nama Lengkap" model="name_req" placeholder="Nama Lengkap..." required />
             <x-form.input-text label="Masukan email kantor anda untuk request pembuatan user login" type='email' model="email_req" placeholder="Masukkan email.." required />
