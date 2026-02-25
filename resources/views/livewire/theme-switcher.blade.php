4704feca6764edcf7dc310f505790fca9d3ce3ec
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-25 14:30:41 +0800

    ds

--- resources/views/livewire/theme-switcher.blade.php
+++ resources/views/livewire/theme-switcher.blade.php
@@ -1,4 +1,4 @@
-<div class="fab bg-base-100">
+<div class="fab">
     <div tabindex="0" role="button" class="btn btn-xs btn-circle btn-info">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-paintbrush-vertical-icon lucide-paintbrush-vertical">
             <path d="M10 2v2" />
