4f1630b36face723f4898ef644e89184484c7530
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 08:02:47 +0800

    dasdsad

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<html data-theme="{{ session('theme', 'corporate') }}" lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 
 <head>
     @include('partials.head')
 </head>
 
-<body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-    <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-        <div class="flex w-full max-w-sm md:max-w-lg flex-col gap-2">
+<body class="min-h-screen antialiased bg-white dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+    <div class="flex flex-col items-center justify-center gap-6 p-6 bg-background min-h-svh md:p-10">
+        <div class="flex flex-col w-full max-w-sm gap-2 md:max-w-lg">
             <div class="flex flex-col gap-6">
                 {{ $slot }}
             </div>
