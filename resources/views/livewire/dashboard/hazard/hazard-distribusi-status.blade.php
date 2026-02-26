244c20faf8414533e4dd0478de68f4d85523325b
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 09:13:09 +0800

    as

--- resources/views/livewire/dashboard/hazard/hazard-distribusi-status.blade.php
+++ resources/views/livewire/dashboard/hazard/hazard-distribusi-status.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div wire:ignore id="chart-container" style="height: 320px;" class="w-full border bg-base-100"></div>
+    <div wire:ignore id="chart-container" style="height: 320px;" class="w-full border bg-base-100 border-base-200"></div>
 
     <script type="module">
         var dom_status = document.getElementById('chart-container');
