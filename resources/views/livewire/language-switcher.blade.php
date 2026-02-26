bb3324486ac29d1d2c5dd7332a93cf2246ac7a5f
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 10:17:45 +0800

    asd

--- resources/views/livewire/language-switcher.blade.php
+++ resources/views/livewire/language-switcher.blade.php
@@ -1,8 +1,18 @@
 <div class="flex items-center space-x-2">
-
-    <select wire:model.live="lang" name="" id=""class=" select select-xs select-bordered focus-within:outline-none focus-within:border-info focus-within:ring-0">
-         <option value="">{{ __('Pilih Bahasa') }}</option>
-         <option value="en"><i class="inline-block w-4 h-4 text-2xl fi fi-gb"></i>{{ __('English') }}</option>
-         <option value="id"><i class="inline-block w-4 h-4 text-2xl fi fi-id"></i></i>{{ __('Bahasa Indonesia') }}</option>
-    </select>
+    <div class="join">
+         <label class="rounded-r-full btn btn-xs btn-primary join-item">
+            @if (session('lang') == 'en')
+                <i class="inline-block w-4 h-4 text-2xl fi fi-gb"></i>
+            @elseif (session('lang') == 'id')
+                <i class="inline-block w-4 h-4 text-2xl fi fi-id"></i>
+            @else
+                {{ __('Pilih Bahasa') }}
+            @endif
+         </label>
+        <select wire:model.live="lang" name="" id="" class=" select select-xs select-bordered focus-within:outline-none focus-within:border-info focus-within:ring-0">
+            <option value="">{{ __('Pilih Bahasa') }}</option>
+            <option value="en"><i class="inline-block w-4 h-4 text-2xl fi fi-gb"></i>{{ __('English') }}</option>
+            <option value="id"><i class="inline-block w-4 h-4 text-2xl fi fi-id"></i></i>{{ __('Bahasa Indonesia') }}</option>
+        </select>
+    </div>
 </div>
