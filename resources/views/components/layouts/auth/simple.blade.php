9e7a3d69b8ad0f9879303362447c5b89e3d88408
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 15:22:57 +0800

    asdasd

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html data-theme="{{ session('theme', 'corporate') }}" lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<html data-theme="{{ session('theme', 'goldore-light') }}" lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 
 <head>
     @include('partials.head')
 </head>
 
-<body class="min-h-screen antialiased bg-white dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+<body class="min-h-screen antialiased bg-linear-to-b from-neutral-950 to-neutral-900">
     <div class="flex flex-col items-center justify-center gap-6 p-6 bg-background min-h-svh md:p-10">
         <div class="flex flex-col w-full max-w-sm gap-2 md:max-w-lg">
             <div class="flex flex-col gap-6">
