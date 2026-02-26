95442ab9da2e6f4546bc554715761cc8d5db988a
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 14:30:18 +0800

    XCZ

--- resources/views/flux/navlist/group-list.blade.php
+++ resources/views/flux/navlist/group-list.blade.php
@@ -2,38 +2,53 @@
 'expandable' => false,
 'expanded' => true,
 'heading' => null,
-'route'=>null,
+'route' => null,
 ])
 
-<?php if ($expandable && $heading): ?>
-<ui-disclosure {{ $attributes->class('group/disclosure') }} {{ Request::is($route) ? 'open' : '' }} data-flux-navlist-group>
-    <button type="button" class="w-full {{ Request::is($route) ? 'bg-neutral text-(--color-neutral-content)' : '' }}  h-10 lg:h-8 flex items-center group/disclosure-button mb-[2px] rounded-sm hover:bg-zinc-800/5 dark:hover:bg-accent  hover:text-(--color-neutral-content)">
+@if ($expandable && $heading)
+@php
+$isActive = Request::is($route);
+@endphp
+<ui-disclosure {{ $attributes->class('group/disclosure') }} {{ $isActive ? 'open' : '' }} data-flux-navlist-group>
+    <button type="button"
+        class="w-full h-10 lg:h-8 flex items-center group/disclosure-button mb-[2px] rounded-sm transition-colors
+            {{ $isActive
+                ? 'bg-neutral text-neutral-content font-semibold'
+                : 'text-base-content/80 hover:bg-accent hover:text-accent-content'
+            }}">
+
         <div class="ps-3 pe-4">
-            <flux:icon.chevron-down class="size-3!  hidden group-data-open/disclosure-button:block" />
-            <flux:icon.chevron-right class="size-3! block group-data-open/disclosure-button:hidden" />
+            {{-- Toggle icon berdasarkan state open/closed --}}
+            <flux:icon.chevron-down class="size-3! hidden group-data-open/disclosure:block" />
+            <flux:icon.chevron-right class="size-3! block group-data-open/disclosure:hidden" />
         </div>
 
-        <span class="text-xs font-medium leading-none {{ Request::is($route) ? 'text-(--color-neutral-content)  font-semibold' : '' }}">{{ $heading }}</span>
+        <span class="text-xs font-medium leading-none">{{ $heading }}</span>
     </button>
 
     <div class="relative hidden data-open:block space-y-[2px] ps-7" @if ($expanded===true) data-open @endif>
-        <div class="absolute inset-y-[3px] w-px bg-zinc-200 dark:bg-(--color-neutral-content)/30 start-0 ms-4"></div>
+        {{-- Garis indikator vertikal (Sub-menu line) --}}
+        <div class="absolute inset-y-[3px] w-px bg-base-content/10 start-0 ms-4"></div>
 
         {{ $slot }}
     </div>
 </ui-disclosure>
-<?php elseif ($heading): ?>
+
+@elseif ($heading)
 <div {{ $attributes->class('block space-y-[2px]') }}>
     <div class="px-3 py-2">
-        <div class="text-sm text-zinc-400 font-medium leading-none">{{ $heading }}</div>
+        {{-- Header statis jika tidak expandable --}}
+        <div class="text-xs font-bold leading-none tracking-wider uppercase text-base-content/50">
+            {{ $heading }}
+        </div>
     </div>
 
     <div>
         {{ $slot }}
     </div>
 </div>
-<?php else: ?>
+@else
 <div {{ $attributes->class('block space-y-[2px]') }}>
     {{ $slot }}
 </div>
-<?php endif; ?>
+@endif
