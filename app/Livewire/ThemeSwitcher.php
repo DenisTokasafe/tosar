783a23cdf17acbbd1176e33eed57a2c77c47e583
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 12:15:36 +0800

    wwre

--- app/Livewire/ThemeSwitcher.php
+++ app/Livewire/ThemeSwitcher.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Session;
 
 class ThemeSwitcher extends Component
 {
-    public $themes = ['fancytide','unknownpenguin', 'corporate', 'wireframe','cookedzombie'];
+    public $themes = ['vibebutnew','fancytide','unknownpenguin', 'corporate', 'wireframe','cookedzombie'];
 
     public function setTheme($theme)
     {
