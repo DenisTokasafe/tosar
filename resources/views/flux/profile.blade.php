a31fe3e0ea2daa247ca2d415350dc944a28cc97e
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 15:48:38 +0800

    sdfsfd

--- resources/views/flux/profile.blade.php
+++ resources/views/flux/profile.blade.php
@@ -2,35 +2,36 @@
 @php $iconTrailing = $iconTrailing ??= $attributes->pluck('icon:trailing'); @endphp
 
 @props([
-    'iconVariant' => 'micro',
-    'iconTrailing' => null,
-    'initials' => null,
-    'chevron' => true,
-    'circle' => null,
-    'avatar' => null,
-    'name' => null,
+'iconVariant' => 'micro',
+'iconTrailing' => null,
+'initials' => null,
+'chevron' => true,
+'circle' => null,
+'avatar' => null,
+'name' => null,
 ])
 
 @php
 $iconTrailing = $iconTrailing ?? ($chevron ? 'chevron-down' : null);
 
-// If no initials are provided, we'll try to generate them from the name by taking the first letter of the first and last name...
 $initials ??= collect(explode(' ', $name ?? ''))
-    ->map(fn($part) => Str::substr($part, 0, 1))
-    ->filter()
-    ->only([0, count(explode(' ', $name ?? '')) - 1])
-    ->implode('');
+->map(fn($part) => Str::substr($part, 0, 1))
+->filter()
+->only([0, count(explode(' ', $name ?? '')) - 1])
+->implode('');
 
-// When using the outline icon variant, we need to size it down to match the default icon sizes...
-$iconClasses = Flux::classes('text-zinc-400 dark:text-white/80 group-hover:text-zinc-800 dark:group-hover:text-white')
-    ->add($iconVariant === 'outline' ? 'size-4' : '');
+// Warna Icon: Menggunakan base-content dengan opasitas (seperti debu mineral)
+// Saat di-hover, icon akan menyala menjadi warna emas (primary)
+$iconClasses = Flux::classes('text-[var(--color-base-content)]/60 group-hover:text-[var(--color-primary)] transition-colors duration-200')
+->add($iconVariant === 'outline' ? 'size-4' : '');
 
 $classes = Flux::classes()
-    ->add('group flex items-center')
-    ->add('rounded-lg has-data-[circle=true]:rounded-full')
-    ->add('[ui-dropdown>&]:w-full') // Without this, the "name" won't get truncated in a sidebar dropdown...
-    ->add('p-1 hover:bg-zinc-800/5 dark:hover:bg-white/10')
-    ;
+->add('group flex items-center transition-all duration-200')
+->add('rounded-lg has-data-[circle=true]:rounded-full')
+->add('[ui-dropdown>&]:w-full')
+/* Hover state: Menggunakan background tipis dari base-content */
+->add('p-1 hover:bg-[var(--color-base-content)]/5 border border-transparent hover:border-[var(--color-primary)]/10')
+;
 @endphp
 
 <button type="button" {{ $attributes->class($classes) }} data-flux-profile>
@@ -38,18 +39,27 @@ $classes = Flux::classes()
         <?php if ($avatar instanceof \Illuminate\View\ComponentSlot): ?>
             {{ $avatar }}
         <?php else: ?>
-            <flux:avatar :attributes="Flux::attributesAfter('avatar:', $attributes, ['src' => $avatar, 'size' => 'xs', 'circle' => $circle, 'name' => $name, 'initials' => $initials])" />
+            {{-- Avatar akan mewarisi warna tema melalui props yang dikirim --}}
+            <flux:avatar :attributes="Flux::attributesAfter('avatar:', $attributes, [
+                'src' => $avatar,
+                'size' => 'xs',
+                'circle' => $circle,
+                'name' => $name,
+                'initials' => $initials,
+                'class' => 'bg-[var(--color-base-300)] text-[var(--color-base-content)] border border-[var(--color-primary)]/20'
+            ])" />
         <?php endif; ?>
     </div>
 
     <?php if ($name): ?>
-        <span class="mx-1 text-xs text-zinc-500 dark:text-neutral-content group-hover:text-zinc-800 dark:group-hover:text-neutral font-medium capitalize truncate">
+        {{-- Nama: Menggunakan warna base-content (Arang/Kapur) dan berubah menjadi Emas saat hover --}}
+        <span class="mx-2 text-xs text-[var(--color-base-content)]/80 group-hover:text-[var(--color-primary)] font-semibold capitalize truncate transition-colors duration-200">
             {{ $name }}
         </span>
     <?php endif; ?>
 
     <?php if (is_string($iconTrailing) && $iconTrailing !== ''): ?>
-        <div class="shrink-0 ms-auto size-8 flex justify-center items-center">
+        <div class="flex items-center justify-center shrink-0 ms-auto size-8">
             <flux:icon :icon="$iconTrailing" :variant="$iconVariant" :class="$iconClasses" />
         </div>
     <?php elseif ($iconTrailing): ?>
