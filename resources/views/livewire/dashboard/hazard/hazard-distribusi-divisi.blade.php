0d4e23ced7dbf8786c5d2981724d01769ebd84fe
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 09:38:54 +0800

    afd

--- resources/views/livewire/dashboard/hazard/hazard-distribusi-divisi.blade.php
+++ resources/views/livewire/dashboard/hazard/hazard-distribusi-divisi.blade.php
@@ -1,5 +1,5 @@
 <div>
-<div class="" wire:ignore id="container" style="height: 355px; width: 100%;"></div>
+<div class="" wire:ignore id="container" style="height: 355px; width: 50%;"></div>
     <script type="module">
         var dom_divis = document.getElementById('container');
         const categories = @json($categories);
