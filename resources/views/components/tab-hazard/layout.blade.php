993b85571599f463bf4b982f1e276e34689c5f0b
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-26 14:48:05 +0800

    ssfdds

--- resources/views/components/tab-hazard/layout.blade.php
+++ resources/views/components/tab-hazard/layout.blade.php
@@ -1,12 +1,16 @@
-<div class="z-30 flex items-start max-md:flex-col ">
-    <div class="self-stretch flex-1 max-md:pt-3">
-        <div class="flex w-full flex-1 flex-col gap-4 rounded-xl h-full
-            max-h-[calc(100vh-20rem)]
-            sm:max-h-[calc(100vh-11rem)]
-            md:max-h-[calc(100vh-16rem)]
-            lg:max-h-[calc(100vh-18rem)]
-            2xl:max-h-[calc(100vh-18rem)]">
-            <div class="flex-1 h-full px-4 py-2 overflow-x-hidden overflow-y-auto border inset-shadow-sm rounded-xl border-neutral-200 dark:border-base-200">
+<div class="z-30 flex flex-col items-start w-full min-h-screen p-4 md:flex-row">
+    <div class="self-stretch flex-1 w-full">
+        <div class="flex flex-col w-full h-full gap-4 rounded-xl
+            /* Mobile (Android) */
+            max-h-[calc(100vh-12rem)]
+            /* Tablet/Small Laptop */
+            md:max-h-[calc(100vh-10rem)]
+            /* PC / Desktop */
+            lg:max-h-[calc(100vh-8rem)]
+            /* Large PC (Ultra Wide) */
+            2xl:max-h-[calc(100vh-6rem)]">
+
+            <div class="flex-1 h-full px-4 py-2 overflow-x-hidden overflow-y-auto bg-white border shadow-sm rounded-xl border-neutral-200 dark:border-neutral-800 dark:bg-neutral-900">
                 <div class="w-full">
                     {{ $slot }}
                 </div>
