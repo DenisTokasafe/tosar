245bb2156878ecb868c1d603327e243cc42d1a8b
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-27 09:20:23 +0800

    df

--- resources/views/livewire/incident/create.blade.php
+++ resources/views/livewire/incident/create.blade.php
@@ -4,7 +4,7 @@
     <div class="flex justify-start mb-2 " wire:ignore>
         {{ Breadcrumbs::render('incident-create') }}
     </div>
-    <x-incident.layout></x-incident.layout>
+    <x-incident.layout>
     <flux:heading level="1" class="mb-1 capitalize">>Buat Laporan Insiden</flux:heading>
     <flux:subheading size="sm" class="mb-1 text-accent">Laporkan insiden dengan detail untuk penanganan yang tepat.</flux:subheading>
     <flux:separator variant="subtle" class="mb-4" />
