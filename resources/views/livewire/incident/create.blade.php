b08f71ec74cb6765a6e433279043c41f9daac75c
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-02-27 14:00:56 +0800

    ds

--- resources/views/livewire/incident/create.blade.php
+++ resources/views/livewire/incident/create.blade.php
@@ -6,7 +6,6 @@
     </div>
     <flux:heading level="1" class="mb-1 capitalize">Buat Laporan Insiden</flux:heading>
     <flux:subheading size="sm" class="mb-1 text-accent">Laporkan insiden dengan detail untuk penanganan yang tepat.</flux:subheading>
-    <flux:separator variant="subtle" class="mb-4" />
     {{-- PROGRESS & STEPS VISUAL --}}
 
     <x-incident.layout>
