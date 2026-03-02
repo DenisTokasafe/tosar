458b43de27d14d1eccc6230c2d4ecd82080218f3
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-02 15:55:32 +0800

    sdg

--- resources/views/components/form/searchable-select-advanced.blade.php
+++ resources/views/components/form/searchable-select-advanced.blade.php
@@ -21,71 +21,85 @@
     <x-form.label :label="$label" :required="$required" />
     @endif
 
-   <div class="relative" x-data="{ open: @entangle($attributes->wire('model') . '.live') }">
+    {{-- Update bagian input dan dropdown di component view Anda --}}
+    <div class="relative" x-data="{
+    open: @entangle($attributes->wire('model') . '.live'),
+    triggerEl: null,
+    dropdownStyles: { top: '0px', left: '0px', width: '0px' },
+    calculatePosition() {
+        const rect = this.$refs.inputField.getBoundingClientRect();
+        this.dropdownStyles = {
+            top: (rect.bottom + window.scrollY) + 'px',
+            left: (rect.left + window.scrollX) + 'px',
+            width: rect.width + 'px'
+        };
+    }
+}" x-init="triggerEl = $refs.inputField">
+
         {{-- Input Search --}}
-        <input {{ $disabled ? 'disabled' : '' }} type="text" wire:model.live.debounce.300ms="{{ $modelsearch }}"
-            placeholder="{{ __($placeholder) }}" x-on:focus="open = true"
+        <input
+            x-ref="inputField"
+            {{ $disabled ? 'disabled' : '' }}
+            type="text"
+            wire:model.live.debounce.300ms="{{ $modelsearch }}"
+            placeholder="{{ __($placeholder) }}"
+            x-on:focus="open = true; calculatePosition()"
+            x-on:input="calculatePosition()"
             {{ $attributes->merge([
-                'class' =>
-                    'input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs ' .
-                    ($disabled ? 'bg-base-200 opacity-70 ' : '') .
-                    ($errors->has($modelid) || ($manualModelName && $errors->has($manualModelName))
-                        ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500'
-                        : ''),
-            ]) }} />
+            'class' => 'input input-bordered w-full input-xs ' . ($disabled ? 'bg-base-200 opacity-70' : '')
+        ]) }} />
 
-        {{-- Dropdown --}}
-        @if (!$disabled && $showdropdown)
-            <ul x-show="open"
-                class="absolute z-[9999] w-full mt-1 overflow-auto border rounded-md shadow bg-base-100 max-h-60">
+        {{-- Teleport Dropdown ke Body --}}
+        <template x-teleport="body">
+            <div
+                x-show="open"
+                x-on:click.outside="open = false"
+                :style="dropdownStyles"
+                class="fixed z-[9999] overflow-hidden border rounded-md shadow-lg bg-base-100"
+                style="display: none;">
+                <ul class="overflow-auto max-h-60">
+                    <div wire:loading wire:target="{{ $clickaction }}" class="flex flex-col items-center justify-center px-4 py-2">
+                        <span class="loading loading-spinner loading-sm text-secondary"></span>
+                    </div>
 
-                <div wire:loading wire:target="{{ $clickaction }}, {{ $enableManualAction }}" class="flex flex-col items-center justify-center px-4 py-2 text-center">
-                    <span class="loading loading-spinner loading-sm text-secondary"></span>
-                </div>
-
-                @if (count($options) > 0)
+                    @if (count($options) > 0)
                     @foreach ($options as $opt)
-                        <li wire:click="{{ $clickaction }}({{ $opt->id }}, '{{ addslashes($opt->{$columnName}) }}')"
-                            wire:key="opt-{{ $opt->id }}" x-on:click="open = false"
-                            class="px-3 py-1 text-sm cursor-pointer hover:bg-base-200">
-                            {{ $opt->{$columnName} }}
-                        </li>
+                    <li wire:click="{{ $clickaction }}({{ $opt->id }}, '{{ addslashes($opt->{$columnName}) }}')"
+                        wire:key="opt-{{ $opt->id }}"
+                        x-on:click="open = false"
+                        class="px-3 py-1 text-sm cursor-pointer hover:bg-base-200">
+                        {{ $opt->{$columnName} }}
+                    </li>
                     @endforeach
-                @else
-                    {{-- Mode Manual Trigger --}}
+                    @else
                     @if (!$manualMode)
-                        <li wire:click="{{ $enableManualAction }}"
-                            class="px-3 py-2 text-sm italic cursor-pointer text-warning hover:bg-base-200">
-                            {{ __('Tidak ditemukan, klik untuk tambah manual') }}
-                        </li>
+                    <li wire:click="{{ $enableManualAction }}"
+                        class="px-3 py-2 text-sm italic cursor-pointer text-warning hover:bg-base-200">
+                        {{ __('Tidak ditemukan, klik untuk tambah manual') }}
+                    </li>
                     @endif
-                @endif
+                    @endif
 
-                {{-- Input Manual Field --}}
-                @if ($manualMode)
-                    <li class="p-2 border-t bg-base-50">
+                    {{-- Input Manual Field --}}
+                    @if ($manualMode)
+                    <li class="p-2 border-t bg-base-200">
                         <div class="flex items-center gap-1">
                             <input type="text" wire:model.live="{{ $manualModelName }}"
-                                placeholder="{{ __('Masukkan nama manual...') }}"
-                                class="w-full input input-bordered input-xs focus:ring-1 focus:ring-info" />
+                                class="w-full input input-bordered input-xs" />
                             <button type="button" wire:click="{{ $addManualAction }}" class="btn btn-primary btn-xs">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M12 4v16m8-8H4" />
-                                </svg>
-                                {{ __('Tambah') }}
+                                Tambah
                             </button>
                         </div>
                     </li>
-                @endif
-            </ul>
-        @endif
+                    @endif
+                </ul>
+            </div>
+        </template>
     </div>
 
     @if ($manualMode && $manualModelName)
-        <x-label-error :messages="$errors->get($manualModelName)" />
+    <x-label-error :messages="$errors->get($manualModelName)" />
     @else
-        <x-label-error :messages="$errors->get($modelid)" />
+    <x-label-error :messages="$errors->get($modelid)" />
     @endif
 </fieldset>
