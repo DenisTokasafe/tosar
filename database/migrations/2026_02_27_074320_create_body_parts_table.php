029d4adb3cea931f5ebb8e4b9d1cb30b1e9fd83b
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-02 10:49:58 +0800

    sdf

--- database/migrations/2026_02_27_074320_create_body_parts_table.php
+++ database/migrations/2026_02_27_074320_create_body_parts_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('body_parts', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('name'); // Nama bagian tubuh (Bahasa Indonesia)
             $table->string('name_en')->nullable(); // Nama (Bahasa Inggris untuk standarisasi jika perlu)
             $table->string('category'); // Kelompok besar: Kepala, Tubuh Atas, dll.
