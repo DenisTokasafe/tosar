40bc82dff1b7d2151cd400893cafe6e1b5b2b6a8
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-02 13:21:04 +0800

    ds

--- resources/views/livewire/administration/part-of-body/index.blade.php
+++ resources/views/livewire/administration/part-of-body/index.blade.php
@@ -1,7 +1,7 @@
 <section class="w-full">
     <x-toast />
 
-    <x-body.layout heading=" __('Manajemen Bagian Tubuh')" subheading="__('Kelola bagian tubuh yang terkait dengan insiden, termasuk nama, kategori, dan kode.')">
+    <x-body.layout heading=" {{ __('Manajemen Bagian Tubuh') }}" subheading="{{ __('Kelola bagian tubuh yang terkait dengan insiden, termasuk nama, kategori, dan kode.') }}}">
 
     </x-body.layout>
 </section>
