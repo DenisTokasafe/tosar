88d20f7f23cc62ae9f4e3301b6ccc20a1ded062e
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-03 14:29:41 +0800

    adasd

--- resources/views/livewire/incident/create.blade.php
+++ resources/views/livewire/incident/create.blade.php
@@ -26,12 +26,12 @@
             @endif
             <x-form.select label="Tipe Insiden" model="event_type_id" :options="$eventTypes" option-label="event_type_name" required />
             <x-form.select label="Jenis Insiden" model="event_sub_type_id" :options="$eventSubTypes" option-label="event_sub_type_name" required />
-            <x-form.select-categroy-bahaya :key-word="$keyWord" :ktas="$ktas" :ttas="$ttas" model_kta="kondisi_tidak_aman" model_tta="tindakan_tidak_aman" required/>
+            <x-form.select-categroy-bahaya :key-word="$keyWord" :ktas="$ktas" :ttas="$ttas" model_kta="kondisi_tidak_aman" model_tta="tindakan_tidak_aman" required />
             <x-form.searchable-select-advanced label="Dilaporkan Oleh" placeholder="Cari Nama Pelapor..."
-                        modelsearch="searchPelapor" modelid="pelapor_id" {{-- ID asli di DB --}} :options="$pelapors"
-                        :showdropdown="$showPelaporDropdown" {{-- Logic Manual --}} :manualMode="$manualPelaporMode"
-                        manualModelName="manualPelaporName" enableManualAction="enableManualPelapor"
-                        addManualAction="addPelaporManual" clickaction="selectPelapor" />
+                modelsearch="searchPelapor" modelid="pelapor_id" {{-- ID asli di DB --}} :options="$pelapors"
+                :showdropdown="$showPelaporDropdown" {{-- Logic Manual --}} :manualMode="$manualPelaporMode"
+                manualModelName="manualPelaporName" enableManualAction="enableManualPelapor"
+                addManualAction="addPelaporManual" clickaction="selectPelapor" />
         </div>
         @endif
         {{-- STEP 2: Detail Kejadian --}}
