62f2ec4cb2cd824272c8ac01b63924f933ef7803
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-03 16:06:33 +0800

    asdsdads

--- resources/views/livewire/incident/create.blade.php
+++ resources/views/livewire/incident/create.blade.php
@@ -174,13 +174,15 @@
             @endif
             <flux:separator variant="subtle" class="my-4" />
             <x-form.text_area label="Kronologi Kejadian" model="description" placeholder="Jelaskan Kronologi Kejadian" required />
-            <flux:separator class="text-base-content" text="{{ __('Bagian Tubuh yang Terluka') }}" />
-            <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
-                <x-form.select label="Kategori Bagian Tubuh" model="selectedBodyPartCategory" :options="$this->existingCategory" option-value="category" option-label="category" placeholder="-- {{__('Pilih Kategori Bagian Tubuh')}} --" required />
-                @if ($selectedBodyPartCategory)
-                <x-form.select label="Detail Bagian Tubuh" model="selectedBodyPart" :options="$detailsBodyPart" option-label="display_name" placeholder="-- {{__('Pilih Detail Bagian Tubuh')}} --" required />
-                @endif
-            </div>
+            <fieldset class="p-3 my-4 border shadow-md border-base-100 fieldset card bg-base-100">
+                <legend class="text-sm font-semibold card-title ">{{ __('Bagian Tubuh yang Terluka') }}</legend>
+                <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
+                    <x-form.select label="Kategori Bagian Tubuh" model="selectedBodyPartCategory" :options="$this->existingCategory" option-value="category" option-label="category" placeholder="-- {{__('Pilih Kategori Bagian Tubuh')}} --" required />
+                    @if ($selectedBodyPartCategory)
+                    <x-form.select label="Detail Bagian Tubuh" model="selectedBodyPart" :options="$detailsBodyPart" option-label="display_name" placeholder="-- {{__('Pilih Detail Bagian Tubuh')}} --" required />
+                    @endif
+                </div>
+            </fieldset>
         </div>
 
         @endif
