8b1893482a78f41e5961884a93b786bb735b4b1a
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-03 09:21:11 +0800

    sad

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import "flatpickr/dist/flatpickr.min.css";
 import "flatpickr/dist/plugins/monthSelect/style.css";
 import * as echarts from 'echarts';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+window.ClassicEditor = ClassicEditor;
 import Anchor from '@alpinejs/anchor'
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 
@@ -14,8 +15,8 @@ import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.e
 Alpine.plugin(Anchor)
 
 // Start Livewire
+Livewire.start()
 
-window.ClassicEditor = ClassicEditor;
 window.echarts = echarts
 window.flatpickr = flatpickr;
 window.monthSelectPlugin = monthSelectPlugin.default || monthSelectPlugin;
