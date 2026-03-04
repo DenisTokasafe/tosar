22146b42c55938292027ff8ef6bf02b18aae5dd0
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-04 13:23:19 +0800

    adas

--- resources/views/livewire/incident/create.blade.php
+++ resources/views/livewire/incident/create.blade.php
@@ -196,6 +196,7 @@
             @endif
             <flux:separator variant="subtle" class="my-4" />
             <x-form.text_area label="Narasi detail mengenai urutan kejadian (5W+1H)" model="description" placeholder="{{ __('Contoh: Siapa yang terlibat, Apa yang terjadi, Dimana, Kapan, Mengapa, dan Bagaimana urutannya.')}}" required />
+            <x-form.text_area label="Tindakan Darurat" model="emergency_action" placeholder="{{ __('Jelaskan tindakan segera yang dilakukan setelah kejadian...')}}" required />
             <fieldset class="p-3 my-4 border shadow-md border-base-300 fieldset card bg-base-100">
                 <legend class="text-sm font-semibold card-title ">{{ __('Bagian Tubuh yang Terluka') }}</legend>
                 <div @class([ 'grid grid-cols-1 gap-2' , 'md:grid-cols-2'=> $selectedBodyPartCategory,
