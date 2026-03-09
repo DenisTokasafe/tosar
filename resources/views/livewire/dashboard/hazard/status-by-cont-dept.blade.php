0681074c5b83d491d9a76a90ed981721bb1a3fbe
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-09 13:10:49 +0800

    sdf

--- resources/views/livewire/dashboard/hazard/status-by-cont-dept.blade.php
+++ resources/views/livewire/dashboard/hazard/status-by-cont-dept.blade.php
@@ -28,12 +28,12 @@
         var option = {
             backgroundColor: 'transparent', // Agar menyatu dengan background
             title: {
-                text: 'Status Laporan per Departemen/Kontraktor',
+                text: 'Status Laporan Responsible Departemen/Kontraktor',
                 left: 'center',
                 textStyle: {
                     color: theme.content, // Dinamis
                     fontFamily: 'Poppins, sans-serif',
-                     fontSize: 14
+                    fontSize: 14
                 },
                 subtext: rawData.range ? 'Periode: ' + rawData.range : '12 Bulan Terakhir',
                 subtextStyle: {
@@ -45,13 +45,15 @@
             tooltip: {
                 trigger: 'axis',
                 backgroundColor: theme.base100, // Dinamis
-                borderColor: theme.primary,     // Dinamis
+                borderColor: theme.primary, // Dinamis
                 borderWidth: 1,
                 textStyle: {
-                    color: theme.content       // Dinamis
+                    color: theme.content // Dinamis
                 },
-                axisPointer: { type: 'shadow' },
-                formatter: function (params) {
+                axisPointer: {
+                    type: 'shadow'
+                },
+                formatter: function(params) {
                     let res = '<b>' + params[0].name + '</b>';
                     let total = 0;
                     params.forEach(item => {
@@ -76,8 +78,11 @@
                 bottom: '15%',
                 containLabel: true
             },
-            dataZoom: [
-                { type: 'inside', start: 0, end: 100 },
+            dataZoom: [{
+                    type: 'inside',
+                    start: 0,
+                    end: 100
+                },
                 {
                     show: true,
                     type: 'slider',
@@ -85,7 +90,9 @@
                     start: 0,
                     end: 100,
                     height: 20,
-                    textStyle: { color: theme.content } // Dinamis
+                    textStyle: {
+                        color: theme.content
+                    } // Dinamis
                 }
             ],
             xAxis: {
@@ -101,14 +108,20 @@
                     }
                 },
                 axisLine: {
-                    lineStyle: { color: theme.base300 } // Dinamis
+                    lineStyle: {
+                        color: theme.base300
+                    } // Dinamis
                 }
             },
             yAxis: {
                 type: 'value',
                 name: 'Jumlah Laporan',
-                nameTextStyle: { color: theme.content },
-                axisLabel: { color: theme.content },
+                nameTextStyle: {
+                    color: theme.content
+                },
+                axisLabel: {
+                    color: theme.content
+                },
                 splitLine: {
                     lineStyle: {
                         color: theme.base300, // Dinamis
@@ -116,14 +129,17 @@
                     }
                 }
             },
-            series: [
-                {
+            series: [{
                     name: 'Open',
                     type: 'bar',
                     stack: 'total',
                     barMaxWidth: 40,
-                    itemStyle: { color: '#F87171' }, // Tetap merah (status bahaya)
-                    emphasis: { focus: 'series' },
+                    itemStyle: {
+                        color: '#F87171'
+                    }, // Tetap merah (status bahaya)
+                    emphasis: {
+                        focus: 'series'
+                    },
                     data: rawData.open
                 },
                 {
@@ -135,7 +151,9 @@
                         color: '#34D399',
                         borderRadius: [4, 4, 0, 0]
                     },
-                    emphasis: { focus: 'series' },
+                    emphasis: {
+                        focus: 'series'
+                    },
                     data: rawData.closed
                 }
             ]
@@ -148,27 +166,53 @@
             const newTheme = fetchColors();
             myChart.setOption({
                 title: {
-                    textStyle: { color: newTheme.content },
-                    subtextStyle: { color: newTheme.content }
+                    textStyle: {
+                        color: newTheme.content
+                    },
+                    subtextStyle: {
+                        color: newTheme.content
+                    }
                 },
                 tooltip: {
                     backgroundColor: newTheme.base100,
                     borderColor: newTheme.primary,
-                    textStyle: { color: newTheme.content }
+                    textStyle: {
+                        color: newTheme.content
+                    }
                 },
                 legend: {
-                    textStyle: { color: newTheme.content }
+                    textStyle: {
+                        color: newTheme.content
+                    }
                 },
                 xAxis: {
-                    axisLabel: { color: newTheme.content },
-                    axisLine: { lineStyle: { color: newTheme.base300 } }
+                    axisLabel: {
+                        color: newTheme.content
+                    },
+                    axisLine: {
+                        lineStyle: {
+                            color: newTheme.base300
+                        }
+                    }
                 },
                 yAxis: {
-                    nameTextStyle: { color: newTheme.content },
-                    axisLabel: { color: newTheme.content },
-                    splitLine: { lineStyle: { color: newTheme.base300 } }
+                    nameTextStyle: {
+                        color: newTheme.content
+                    },
+                    axisLabel: {
+                        color: newTheme.content
+                    },
+                    splitLine: {
+                        lineStyle: {
+                            color: newTheme.base300
+                        }
+                    }
                 },
-                dataZoom: [{}, { textStyle: { color: newTheme.content } }]
+                dataZoom: [{}, {
+                    textStyle: {
+                        color: newTheme.content
+                    }
+                }]
             });
         });
 
@@ -184,14 +228,21 @@
                 title: {
                     subtext: payload.range ? 'Periode: ' + payload.range : '12 Bulan Terakhir'
                 },
-                xAxis: { data: payload.labels },
-                series: [
-                    { data: payload.open },
-                    { data: payload.closed }
+                xAxis: {
+                    data: payload.labels
+                },
+                series: [{
+                        data: payload.open
+                    },
+                    {
+                        data: payload.closed
+                    }
                 ]
             });
         });
 
-        window.addEventListener('resize', () => { myChart.resize(); });
+        window.addEventListener('resize', () => {
+            myChart.resize();
+        });
     </script>
 </div>
