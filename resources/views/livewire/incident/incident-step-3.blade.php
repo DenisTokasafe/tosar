df354c56e2ba2f963d9b44066b2db699d49b10f4
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-10 15:18:18 +0800

    sass

--- resources/views/livewire/incident/incident-step-3.blade.php
+++ resources/views/livewire/incident/incident-step-3.blade.php
@@ -86,6 +86,8 @@
                 </td>
                 <td class="w-1/4 px-2">
                     <x-form.tgl-waktu label="Batas Waktu Penyelesaian" model="corrective_actions.{{ $index }}.due_date" :min-date="now()->format('Y-m-d\TH:i')" />
+                </td>
+                <td>
                     <x-form.tgl-waktu label="Tanggal Penyelesaian Tindakan" model="corrective_actions.{{ $index }}.actual_completion_date" :min-date="now()->format('Y-m-d\TH:i')" />
                 </td>
                 <td class="w-1/12 pl-4">
