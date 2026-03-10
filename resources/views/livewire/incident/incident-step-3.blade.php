c86fb47d1b0759616d4a472dd514efa6d016b17d
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-10 15:20:31 +0800

    asd

--- resources/views/livewire/incident/incident-step-3.blade.php
+++ resources/views/livewire/incident/incident-step-3.blade.php
@@ -87,10 +87,10 @@
                 <td class="w-1/4 px-2">
                     <x-form.tgl-waktu label="Batas Waktu Penyelesaian" model="corrective_actions.{{ $index }}.due_date" :min-date="now()->format('Y-m-d\TH:i')" />
                 </td>
-                <td class="w-1/4 px-2">
+                <td class="w-12 px-2">
                     <x-form.tgl-waktu label="Tanggal Penyelesaian Tindakan" model="corrective_actions.{{ $index }}.actual_completion_date" :min-date="now()->format('Y-m-d\TH:i')" />
                 </td>
-                <td class="w-1/12 pl-4">
+                <td class="w-12 px-2">
                     @if(count($corrective_actions) > 1)
                     <button type="button" wire:click="removeCorrectiveRow({{ $index }})" class="btn btn-ghost btn-xs text-error">✕</button>
                     @endif
