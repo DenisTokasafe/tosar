09dfe2a859fd0cf1048ccf57b8031d15b7d2cc1b
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-11 09:33:19 +0800

    asd

--- resources/views/livewire/incident/create.blade.php
+++ resources/views/livewire/incident/create.blade.php
@@ -4,53 +4,45 @@
     <div class="flex justify-start mb-2 " wire:ignore>
         {{ Breadcrumbs::render('incident-create') }}
     </div>
-    <flux:heading level="1" class="mb-1 capitalize">{{ __('Buat Laporan Insiden') }}</flux:heading>
-    <flux:subheading size="sm" class="mb-1 text-accent">{{ __('Laporkan insiden dengan detail untuk penanganan yang tepat.') }}</flux:subheading>
+    <flux:heading level="1" class="mb-1 capitalize">{{ __('Buat Laporan Insiden') }}
+    </flux:heading>
+    <flux:subheading size="sm" class="mb-1 text-accent">
+        {{ __('Laporkan insiden dengan detail untuk penanganan yang tepat.') }}
+    </flux:subheading>
     <x-incident.layout>
         {{-- PROGRESS & STEPS VISUAL --}}
-        <ul class="absolute inset-x-0 top-0 z-10 border-t border-l-0 border-r-0 rounded-t-sm shadow-md border-base-300 steps lg:steps-horizontal bg-base-100">
-            <li class="step {{ $currentStep >= 1 ? 'step-primary' : '' }} text-[10px] uppercase font-bold">{{ __('Informasi Dasar') }}</li>
-            <li class="step {{ $currentStep >= 2 ? 'step-primary' : '' }} text-[10px] uppercase font-bold">{{ __('Detail & Risiko') }}</li>
-            <li class="step {{ $currentStep >= 3 ? 'step-primary' : '' }} text-[10px] uppercase font-bold">{{ __('Dokumentasi') }} & {{ __('Tindakan Perbaikan') }}</li>
-        </ul>
+
         {{-- STEP 1: Info Dasar --}}
-        <div class="collapse collapse-arrow bg-base-100 mt-12 border border-base-300">
+        <div class="mt-12 border collapse collapse-arrow bg-base-100 border-base-300">
             <input type="radio" name="my-accordion-2" checked="checked" />
-            <div class="collapse-title font-semibold bg-base-300 text-base-content">
+            <div class="font-semibold collapse-title bg-base-300 text-base-content">
                 <span class="flex">
                     <h3>BAGIAN 1 </h3>- Detail Laporan
                 </span>
             </div>
-            <div class="collapse-content text-sm"> @include('livewire.incident.incident-step-1')</div>
+            <div class="text-sm collapse-content"> @include('livewire.incident.incident-step-1')</div>
         </div>
-        <div class="collapse collapse-arrow bg-base-100 border border-base-300">
+        <div class="border collapse collapse-arrow bg-base-100 border-base-300">
             <input type="radio" name="my-accordion-2" />
-            <div class="collapse-title font-semibold bg-base-300 text-base-content">
+            <div class="font-semibold collapse-title bg-base-300 text-base-content">
                 <span class="flex">
                     <h3>BAGIAN 2 </h3>- Pihak Terlibat Langsung (Saksi, korban cedera, kontraktor, operator, dll.)
                 </span>
             </div>
-            <div class="collapse-content text-sm">@include('livewire.incident.incident-step-2')</div>
+            <div class="text-sm collapse-content">@include('livewire.incident.incident-step-2')</div>
         </div>
-        <div class="collapse collapse-arrow bg-base-100 border border-base-300">
+        <div class="border collapse collapse-arrow bg-base-100 border-base-300">
             <input type="radio" name="my-accordion-2" />
-            <div class="collapse-title font-semibold bg-base-300 text-base-content">
+            <div class="font-semibold collapse-title bg-base-300 text-base-content">
                 <span class="flex">
                     <h3>BAGIAN 3 </h3>- Partisipan Investigasi
                 </span>
             </div>
-            <div class="collapse-content text-sm"> @include('livewire.incident.incident-step-3')</div>
+            <div class="text-sm collapse-content"> @include('livewire.incident.incident-step-3')</div>
         </div>
         {{-- Navigasi Step --}}
         <div class="flex justify-end gap-2 p-2 md:mt-4 bg-base-100">
-            @if($currentStep > 1)
-            <button type="button" class="btn btn-xs btn-outline" wire:click="$set('currentStep', {{ $currentStep - 1 }})">Sebelumnya</button>
-            @endif
-            @if($currentStep < $totalSteps)
-                <button type="button" class="btn btn-xs btn-primary" wire:click="$set('currentStep', {{ $currentStep + 1 }})">Selanjutnya</button>
-                @else
-                <button type="button" class="btn btn-xs btn-success" wire:click="submit">Submit</button>
-                @endif
+            <button type="button" class="btn btn-xs btn-success" wire:click="submit">Submit</button>
         </div>
 
     </x-incident.layout>
