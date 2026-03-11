2341802f317895d07f50ebef5fee7db25b2cdfa9
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-11 10:22:50 +0800

    asd

--- resources/views/livewire/incident/incident-step-2.blade.php
+++ resources/views/livewire/incident/incident-step-2.blade.php
@@ -28,7 +28,8 @@
                         <tr wire:key="involved-{{ $index }}">
                             <td class="align-top">
                                 <x-form.searchable-select-advanced placeholder="Cari Nama..."
-                                    wire:key="employee-search-{{ $index }}" modelsearch="searchKorban.{{ $index }}"
+                                    wire:key="employee_name-field-{{ $index }}-{{ $directly_involved[$index]['employee_name'] ?? 'new' }}"
+                                    modelsearch="searchKorban.{{ $index }}"
                                     modelid="directly_involved.{{ $index }}.employee_name"
                                     :options="$involved_personnel_options"
                                     :showdropdown="$show_employee_dropdown[$index] ?? false"
