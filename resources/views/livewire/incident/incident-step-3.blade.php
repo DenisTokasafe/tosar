3353661ec7940b2af582ec950f5bce66d3e0b51e
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-11 16:17:08 +0800

    asd

--- resources/views/livewire/incident/incident-step-3.blade.php
+++ resources/views/livewire/incident/incident-step-3.blade.php
@@ -27,16 +27,15 @@
                             x-on:focusin="$wire.set('activeType', 'pemimpin'); $wire.set('activeIndex', {{ $index }})" />
                     </td>
                     <td class="p-1 border border-base-300">
-
-                        <x-form.input-text model="pemimpin.{{ $index }}.jabatan"
-                            wire:key="jabatan-field-{{ $index }}-{{ $pemimpin[$index]['jabatan'] ?? 'new' }}"
-                            :disabled="$item['jabatan'] ? true : false" />
-                    </td>
-                    <td class="flex items-center gap-1 p-1 border border-base-300">
-
                         <x-form.input-text model="pemimpin.{{ $index }}.dept"
                             wire:key="dept-field-{{ $index }}-{{ $pemimpin[$index]['dept'] ?? 'new' }}"
                             :disabled="$item['jabatan'] ? true : false" />
+
+                    </td>
+                    <td class="flex items-center gap-1 p-1 border border-base-300">
+                        <x-form.input-text model="pemimpin.{{ $index }}.jabatan"
+                            wire:key="jabatan-field-{{ $index }}-{{ $pemimpin[$index]['jabatan'] ?? 'new' }}" />
+
                         <button type="button" wire:click="addRow('pemimpin')"
                             class="btn btn-xs btn-ghost text-success">+</button>
                         @if(count($pemimpin) > 1)
