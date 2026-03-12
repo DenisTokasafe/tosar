9b935399af0126eabba7a1e31a33bd4213dfa4d8
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-12 10:24:47 +0800

    AS

--- resources/views/livewire/incident/create.blade.php
+++ resources/views/livewire/incident/create.blade.php
@@ -50,6 +50,15 @@
             </div>
             <div class="text-xs collapse-content"> @include('livewire.incident.incident-step-4')</div>
         </div>
+        <div class="border collapse collapse-arrow bg-base-100 border-base-300">
+            <input type="radio" name="my-accordion-2" />
+            <div class="font-semibold collapse-title bg-linear-to-r/oklab from-base-200 to-base-300 text-base-content">
+
+                <h3 class="text-base font-semibold">PART 5 <span class="badge badge-md badge-soft badge-info">Time Line dan Analisis Informasi</span></h3>
+
+            </div>
+            <div class="text-xs collapse-content"> @include('livewire.incident.incident-step-5')</div>
+        </div>
         {{-- Navigasi Step --}}
         <div class="flex justify-end gap-2 p-2 md:mt-4 bg-base-100">
             <button type="button" class="btn btn-xs btn-success" wire:click="submit">Submit</button>
