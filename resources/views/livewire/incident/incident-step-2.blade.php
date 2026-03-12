a0a1b4122b9e773dec9865da8adf227fac8a6091
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-12 09:11:26 +0800

    asd

--- resources/views/livewire/incident/incident-step-2.blade.php
+++ resources/views/livewire/incident/incident-step-2.blade.php
@@ -10,9 +10,9 @@
         <table class="table table-xs w-full border border-base-300">
             <thead>
                 <tr class="bg-gray-200 text-center text-base-content">
-                    <th class="border border-base-300  text-sm font-bold w-1/5">Nama</th>
+                    <th class="border border-base-300  text-sm font-bold w-1/6">Nama</th>
                     <th class="border border-base-300  text-sm font-bold">ID</th>
-                    <th class="border border-base-300  text-sm font-bold">Dept. / Perusahaan</th>
+                    <th class="border border-base-300  text-sm font-bold ">Dept. / Perusahaan</th>
                     <th class="border border-base-300  text-sm font-bold">Jabatan</th>
                     <th class="border border-base-300  text-sm font-bold">Roster</th>
                     <th class="border border-base-300  text-sm font-bold">Shift</th>
