4090982fa7ca3627ce15264b97e8e01274dea549
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-12 11:16:57 +0800

    as

--- resources/views/livewire/incident/incident-step-5.blade.php
+++ resources/views/livewire/incident/incident-step-5.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-4 ">
     <div class="flex items-center justify-between pb-2 border-b">
-        <h2 class="text-lg font-bold">BAGIAN 5 – Time Line dan Analisis Informasi</h2>
+
         <div class="flex gap-2">
             {{-- Tombol Hapus Kolom (Hanya tampil jika > 1) --}}
             @if($whyCount > 1)
@@ -22,12 +22,12 @@
     @foreach($timelines as $index => $line)
     <div class="border shadow-sm card bg-base-100 border-base-300" wire:key="timeline-card-{{ $index }}">
         <div class="p-4 card-body">
-            <div class="flex items-start justify-between mb-4">
+            <!-- <div class="flex items-start justify-between mb-4">
                 <span class="font-mono badge badge-ghost">Kronologi #{{ $index + 1 }}</span>
                 <button type="button" wire:click="removeRow('timelines', {{ $index }})" class="btn btn-xs btn-error btn-outline">
                     Hapus Baris
                 </button>
-            </div>
+            </div> -->
 
             <div class="grid grid-cols-1 gap-4">
                 <div class="w-full form-control">
@@ -52,4 +52,10 @@
         </div>
     </div>
     @endforeach
+
+    <!-- <div class="flex justify-center mt-4">
+        <button type="button" wire:click="addRow('timelines')" class="btn btn-block btn-success btn-outline">
+            + Tambah Baris Kronologi Baru
+        </button>
+    </div> -->
 </div>
