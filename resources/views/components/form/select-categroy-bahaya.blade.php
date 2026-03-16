754e779ba28995151f5e51d613063d71b48ecc84
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-16 09:31:52 +0800

    asdasd

--- resources/views/components/form/select-categroy-bahaya.blade.php
+++ resources/views/components/form/select-categroy-bahaya.blade.php
@@ -34,7 +34,7 @@
     <div wire:key="dropdown-kta-{{ $model_kta }}" class="{{ $keyWord === 'kta' ? 'block' : 'hidden' }} mb-1.5">
         <select {{ $model_kta ? "wire:model.live=$model_kta" : '' }}
             class="select select-xs mb-1 select-bordered w-full focus-within:outline-none focus-within:border-info focus-within:ring-0
-            {{ $errors->has($model_kta) ? 'ring-1 ring-rose-500 border-rose-500' : '' }}">
+            {{ $errors->has($model_kta) ? 'focus:ring-rose-500  focus-within:outline-none focus-within:border-rose-500  focus-within:ring-0' : '' }}">
             <option value="">{{__($options_label)}}</option>
             @foreach ($ktas as $kta)
             <option value="{{ $kta->id }}">{{ __($kta->name) }}</option>
@@ -48,7 +48,7 @@
     <div wire:key="dropdown-tta-{{ $model_tta }}" class="{{ $keyWord === 'tta' ? 'block' : 'hidden' }} mb-1.5">
         <select {{ $model_tta ? "wire:model.live=$model_tta" : '' }}
             class="select select-xs mb-1 select-bordered w-full focus-within:outline-none focus-within:border-info focus-within:ring-0
-            {{ $errors->has($model_tta) ? 'ring-1 ring-rose-500 border-rose-500' : '' }}">
+            {{ $errors->has($model_tta) ? 'focus:ring-rose-500  focus-within:outline-none focus-within:border-rose-500  focus-within:ring-0' : '' }}">
             <option value="">{{__($options_label)}}</option>
             @foreach ($ttas as $tta)
             <option value="{{ $tta->id }}">{{ __($tta->name) }}</option>
