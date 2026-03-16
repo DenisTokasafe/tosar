5ad76573774a45053f10aef1944103c1c2179a68
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-16 08:56:24 +0800

    asdasd

--- resources/views/components/form/select-categroy-bahaya.blade.php
+++ resources/views/components/form/select-categroy-bahaya.blade.php
@@ -9,41 +9,47 @@
 ])
 
 <fieldset {{ $attributes->merge(['class' => 'fieldset']) }}>
-    <div class="flex items-center gap-4 ">
+    <div class="flex items-center gap-4 mb-2">
         <div class="flex items-center gap-1">
             <input id="kta" value="kta" wire:model.live="keyWord"
                 class="peer/kta radio radio-xs radio-accent" type="radio" name="keyWord" />
+            {{-- Perbaikan logika required --}}
             <x-form.label for="kta" class="peer-checked/kta:text-accent text-[10px] cursor-pointer"
-                label="Kondisi Tidak Aman" :required="{{ !$model_tta ? '$required' : '' }}" />
+                label="Kondisi Tidak Aman" :required="!$model_tta && $required" />
         </div>
 
         <div class="flex items-center gap-1">
             <input id="tta" value="tta" wire:model.live="keyWord"
                 class="peer/tta radio radio-xs radio-primary" type="radio" name="keyWord" />
             <x-form.label for="tta" class="peer-checked/tta:text-primary text-[10px] cursor-pointer"
-                label="Tindakan Tidak Aman" :required="{{ !$model_kta ? '$required' : '' }}" />
+                label="Tindakan Tidak Aman" :required="!$model_kta && $required" />
         </div>
     </div>
 
+    {{-- Section KTA --}}
     <div class="{{ $keyWord === 'kta' ? 'block' : 'hidden' }} mb-1.5">
         <select {{ $model_kta ? "wire:model.live=$model_kta" : '' }}
-            class="select select-xs mb-1 select-bordered w-full focus-within:outline-none focus-within:border-info focus-within:ring-0 {{ $errors->has('kondisi_tidak_aman') ? 'ring-1 ring-rose-500 border-rose-500' : '' }}">
+            class="select select-xs mb-1 select-bordered w-full focus-within:outline-none focus-within:border-info focus-within:ring-0
+            {{ $errors->has($model_kta) ? 'ring-1 ring-rose-500 border-rose-500' : '' }}">
             <option value="">{{__($options_label)}}</option>
             @foreach ($ktas as $kta)
             <option value="{{ $kta->id }}">{{ __($kta->name) }}</option>
             @endforeach
         </select>
-        <x-label-error :messages="$errors->get('model_kta')" />
+        {{-- Perbaikan: variabel $model_kta tanpa kutip --}}
+        <x-label-error :messages="$errors->get($model_kta)" />
     </div>
 
+    {{-- Section TTA --}}
     <div class="{{ $keyWord === 'tta' ? 'block' : 'hidden' }} mb-1.5">
         <select {{ $model_tta ? "wire:model.live=$model_tta" : '' }}
-            class="select select-xs mb-1 select-bordered w-full focus-within:outline-none focus-within:border-info focus-within:ring-0 {{ $errors->has('tindakan_tidak_aman') ? 'ring-1 ring-rose-500 border-rose-500' : '' }}">
+            class="select select-xs mb-1 select-bordered w-full focus-within:outline-none focus-within:border-info focus-within:ring-0
+            {{ $errors->has($model_tta) ? 'ring-1 ring-rose-500 border-rose-500' : '' }}">
             <option value="">{{__($options_label)}}</option>
             @foreach ($ttas as $tta)
             <option value="{{ $tta->id }}">{{ __($tta->name) }}</option>
             @endforeach
         </select>
-        <x-label-error :messages="$errors->get('model_tta')" />
+        <x-label-error :messages="$errors->get($model_tta)" />
     </div>
 </fieldset>
