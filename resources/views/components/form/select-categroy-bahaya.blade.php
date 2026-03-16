765d0cbadb7fb4fc176440e8d672c6047ad2a9dd
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-16 10:13:53 +0800

    ASD

--- resources/views/components/form/select-categroy-bahaya.blade.php
+++ resources/views/components/form/select-categroy-bahaya.blade.php
@@ -10,24 +10,24 @@
 
 <fieldset {{ $attributes->merge(['class' => 'fieldset']) }}>
     <div class="flex items-center gap-4 ">
-        {{-- Radio Kondisi Tidak Aman (KTA) --}}
+        {{-- Radio Department --}}
         <div class="flex items-center gap-1">
-            <input id="kta" value="kta" wire:model.live="keyWord"
-                class="peer/kta radio radio-xs radio-accent" type="radio" name="keyWord" />
+            <input id="department" value="department" wire:model.live="model"
+                class="peer/department radio radio-xs radio-accent" type="radio" name="{{ $model }}" />
 
-            <x-form.label for="kta" class="peer-checked/kta:text-accent text-[10px] cursor-pointer"
-                label="Kondisi Tidak Aman"
-                :required="$keyWord === 'kta' && $required" />
+            <x-form.label for="department" class="peer-checked/department:text-accent text-[10px] cursor-pointer"
+                label="{{ $label_dept }}"
+                :required="$model === 'department' && $required" />
         </div>
 
-        {{-- Radio Tindakan Tidak Aman (TTA) --}}
+        {{-- Radio Company/Contractor --}}
         <div class="flex items-center gap-1">
-            <input id="tta" value="tta" wire:model.live="keyWord"
-                class="peer/tta radio radio-xs radio-primary" type="radio" name="keyWord" />
+            <input id="company" value="company" wire:model.live="model"
+                class="peer/company radio radio-xs radio-primary" type="radio" name="{{ $model }}" />
 
-            <x-form.label for="tta" class="peer-checked/tta:text-primary text-[10px] cursor-pointer"
-                label="Tindakan Tidak Aman"
-                :required="$keyWord === 'tta' && $required" />
+            <x-form.label for="company" class="peer-checked/company:text-primary text-[10px] cursor-pointer"
+                label="{{ $label_contractor }}"
+                :required="$model === 'company' && $required" />
         </div>
     </div>
     {{-- Dropdown KTA --}}
