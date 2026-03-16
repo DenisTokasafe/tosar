ffecfe36137f0859e7c5289a262c756aa5916000
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-16 11:15:09 +0800

    sasa

--- resources/views/components/form/department-contractor-selector.blade.php
+++ resources/views/components/form/department-contractor-selector.blade.php
@@ -9,13 +9,13 @@
 'required' => false, // status dropdown contractor
 ])
 
-{{ $model }}
-<fieldset>
+
+<fieldset {{ $attributes->merge(['class' => 'fieldset']) }}>
     <div class="flex items-center gap-4">
         {{-- Radio Department --}}
         <div class="flex items-center gap-1">
             <input id="department" value="department" wire:model.live="model"
-                class="peer/department radio radio-xs radio-accent" type="radio" name="dept_cont_toggle" />
+                class="peer/department radio radio-xs radio-accent" type="radio" name="model" />
 
             <x-form.label for="department" class="peer-checked/department:text-accent text-[10px] cursor-pointer"
                 label="{{ $label_dept }}"
@@ -25,7 +25,7 @@
         {{-- Radio Company/Contractor --}}
         <div class="flex items-center gap-1">
             <input id="company" value="company" wire:model.live="model"
-                class="peer/company radio radio-xs radio-primary" type="radio" name="dept_cont_toggle" />
+                class="peer/company radio radio-xs radio-primary" type="radio" name="model" />
 
             <x-form.label for="company" class="peer-checked/company:text-primary text-[10px] cursor-pointer"
                 label="{{ $label_contractor }}"
@@ -35,19 +35,15 @@
 
     <div wire:key="dropdown-department" class="{{ $model === 'department' ? 'block' : 'hidden' }} mt-1.5">
         {{-- Department --}}
-        <div class="relative ">
-            <x-form.searchable-dropdown-without-label modelsearch="search" modelid="department_id"
-                placeholder="Cari Departemen..." :options="$departments" :showdropdown="$showDropdown"
-                clickaction="selectDepartment" namedb="department_name" />
-        </div>
+        <x-form.searchable-dropdown-without-label modelsearch="search" modelid="department_id"
+            placeholder="Cari Departemen..." :options="$departments" :showdropdown="$showDropdown"
+            clickaction="selectDepartment" namedb="department_name" />
     </div>
     <div wire:key="dropdown-company" class="{{ $model === 'company' ? 'block' : 'hidden' }} mt-1.5">
         {{-- Contractor --}}
-        <div class="relative ">
-            <x-form.searchable-dropdown-without-label modelsearch="searchContractor"
-                placeholder="Cari Kontraktor..." modelid="contractor_id" :options="$contractors"
-                :showdropdown="$showContractorDropdown" clickaction="selectContractor" namedb="contractor_name" />
-        </div>
+        <x-form.searchable-dropdown-without-label modelsearch="searchContractor"
+            placeholder="Cari Kontraktor..." modelid="contractor_id" :options="$contractors"
+            :showdropdown="$showContractorDropdown" clickaction="selectContractor" namedb="contractor_name" />
     </div>
 
 </fieldset>
