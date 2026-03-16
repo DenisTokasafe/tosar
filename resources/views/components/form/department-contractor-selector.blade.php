6a55b7f2c53f7ecb9ba86fffe14594e96c93e701
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-16 11:03:34 +0800

    dg

--- resources/views/components/form/department-contractor-selector.blade.php
+++ resources/views/components/form/department-contractor-selector.blade.php
@@ -1,8 +1,5 @@
 @props([
-'deptCont' => null, // wire:model untuk radio button
-'model_dept'=> null,
-'model_cont'=> null,
-'required' => false,
+'model' => 'deptCont', // wire:model untuk radio button
 'departments' => [],
 'contractors' => [],
 'label_dept' => 'Departemen Terkait',
@@ -11,33 +8,43 @@
 'showContractorDropdown' => false, // status dropdown contractor
 ])
 
+
 <fieldset>
-    <!-- Departemen -->
-    <input id="department" value="department" wire:model.live="deptCont"
-        class="peer/department radio radio-xs radio-accent " type="radio" name="deptCont"
-        checked />
-    <x-form.label for="department" class="peer-checked/department:text-accent text-[10px]"
-        label="{{ $label_dept }}"
-        :required="$deptCont === 'department' && $required" />
-    <!--  Kontraktor -->
-    <input id="kontraktor" value="kontraktor" wire:model.live="deptCont"
-        class="peer/kontraktor radio radio-xs radio-primary" type="radio" name="deptCont" />
-    <x-form.label for="kontraktor" class="peer-checked/kontraktor:text-primary text-[10px]"
-        label="{{ $label_contractor }}"
-        :required="$deptCont === 'kontraktor' && $required" />
+    <div class="flex items-center gap-4">
+        {{-- Radio Department --}}
+        <div class="flex items-center gap-1">
+            <input id="department" value="department" wire:model.live="model"
+                class="peer/department radio radio-xs radio-accent" type="radio" name="dept_cont_toggle" />
+
+            <x-form.label for="department" class="peer-checked/department:text-accent text-[10px] cursor-pointer"
+                label="{{ $label_dept }}"
+                :required="$model === 'department' && $required" />
+        </div>
+
+        {{-- Radio Company/Contractor --}}
+        <div class="flex items-center gap-1">
+            <input id="company" value="company" wire:model.live="model"
+                class="peer/company radio radio-xs radio-primary" type="radio" name="dept_cont_toggle" />
+
+            <x-form.label for="company" class="peer-checked/company:text-primary text-[10px] cursor-pointer"
+                label="{{ $label_contractor }}"
+                :required="$model === 'company' && $required" />
+        </div>
+    </div>
+
     <div class="hidden peer-checked/department:block mt-1.5">
         {{-- Department --}}
         <div class="relative ">
-            <x-form.searchable-dropdown-without-label modelsearch="search" modelid="{{ $model_dept ?$model_dept : '' }}"
+            <x-form.searchable-dropdown-without-label modelsearch="search" modelid="department_id"
                 placeholder="Cari Departemen..." :options="$departments" :showdropdown="$showDropdown"
                 clickaction="selectDepartment" namedb="department_name" />
         </div>
     </div>
-    <div class="hidden peer-checked/kontraktor:block mt-1.5">
+    <div class="hidden peer-checked/company:block mt-1.5">
         {{-- Contractor --}}
         <div class="relative ">
             <x-form.searchable-dropdown-without-label modelsearch="searchContractor"
-                placeholder="Cari Kontraktor..." modelid="{{ $model_cont ?$model_cont : '' }}" :options="$contractors"
+                placeholder="Cari Kontraktor..." modelid="contractor_id" :options="$contractors"
                 :showdropdown="$showContractorDropdown" clickaction="selectContractor" namedb="contractor_name" />
         </div>
     </div>
