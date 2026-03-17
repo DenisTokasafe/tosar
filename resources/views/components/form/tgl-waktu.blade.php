6d323a13f6782bc2fa0459039cf8def58e79e9bb
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-17 09:06:37 +0800

    asd

--- resources/views/components/form/tgl-waktu.blade.php
+++ resources/views/components/form/tgl-waktu.blade.php
@@ -15,11 +15,8 @@
     @endif
 
     {{-- Wrapper Alpine.js untuk Flatpickr --}}
-    <div wire:ignore
-        x-data="{
+    <div x-data="{
         reportDate: @entangle($model),
-        {{-- 1. Deteksi error secara reaktif dari Livewire --}}
-        get hasError() { return @js($errors->has($model)) },
         fp: null,
         init() {
             this.fp = flatpickr(this.$refs.tanggalInput, {
@@ -28,46 +25,31 @@
                 enableTime: true,
                 time_24hr: true,
                 altFormat: '{{ $dateFormat }}',
-                dateFormat: 'Y-m-d H:i', {{-- Sesuaikan format jika enableTime aktif --}}
+                dateFormat: 'Y-m-d',
                 defaultDate: this.reportDate,
                 onChange: (selectedDates, dateStr) => {
                     this.reportDate = dateStr;
                 }
             });
 
-            {{-- 2. Sinkronisasi perubahan dari Livewire ke Flatpickr --}}
             this.$watch('reportDate', (newVal) => {
-                if (newVal !== this.fp.currentSelectedDateString) {
-                    this.fp.setDate(newVal, false);
-                }
+                this.fp.setDate(newVal, false);
             });
         }
-    }"
-        {{-- 3. Gunakan Alpine untuk manipulasi class secara reaktif --}}
-        :class="hasError ? 'flatpickr-error' : ''">
+    }">
+        {{-- Input Element --}}
         <input
             x-ref="tanggalInput"
             type="text"
             readonly
             {{ $disabled ? 'disabled' : '' }}
+            {{ $model ? "wire:model.live.debounce.300ms=$model" : '' }}
             placeholder="{{ $placeholder ?: $label }}"
             {{ $attributes->merge([
-            'class' => 'input input-bordered w-full input-xs focus:outline-none focus:border-info focus:ring-0',
-        ]) }}
-            {{-- CSS khusus untuk menangani border merah pada alt-input Flatpickr --}}
-            :class="hasError ? '!border-rose-500 !ring-1 !ring-rose-500' : ''" />
+            'class' => "input input-bordered w-full focus-within:outline-none focus-within:border-info focus-within:ring-0 $size border-gray-300  " .
+            ($errors->has($model) ? 'border-rose-500 focus-within:border-rose-500' : '')
+        ]) }} />
     </div>
-
-    <style>
-        /* Paksa input yang dibuat flatpickr (alt-input) ikut berwarna merah */
-        .flatpickr-error+.form-control,
-        .flatpickr-error .flatpickr-mobile,
-        .flatpickr-error input.altInput {
-            border-color: #f43f5e !important;
-            /* rose-500 */
-            box-shadow: 0 0 0 1px #f43f5e !important;
-        }
-    </style>
 
     {{-- Penanganan Error Otomatis --}}
     @if($model)
