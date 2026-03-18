b9440c4a8626bce0b94901aa100e1397baf0c170
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-18 11:51:49 +0800

    asdasd

--- resources/views/components/form/upload.blade.php
+++ resources/views/components/form/upload.blade.php
@@ -21,7 +21,7 @@
 
         <span @class([ 'btn btn-xs' , 'btn-info'=> !$disabled,
             'btn-disabled bg-gray-300 text-gray-500 border-none' => $disabled,
-            'btn-disabled bg-error text-error-component border-none' => $disabled
+            'btn-disabled bg-error text-error-component border-none' => $errors->has($model)
             ])>
             {{ __($title) }}
         </span>
