be2162c74c17aedf83b58e88a350ee41de2a439b
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-23 09:45:19 +0800

    sasa

--- resources/views/livewire/language-switcher.blade.php
+++ resources/views/livewire/language-switcher.blade.php
@@ -1,12 +1,12 @@
-<div class="flex items-center space-x-2">
-    <div class="join">
-        <div class=" badge badge-outline badge-md join-item badge-primary-content">
+<div class="flex items-center">
+    <div class="flex items-stretch join">
+        <div class="flex items-center justify-center px-3 border border-r-0 border-base-300 join-item bg-base-200">
             @if ($lang === 'en')
-            <i class="fi fi-gb"></i>
+            <span class="fi fi-gb"></span>
             @elseif ($lang === 'id')
-            <i class="fi fi-id"></i>
+            <span class="fi fi-id"></span>
             @else
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-languages-icon lucide-languages">
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-languages">
                 <path d="m5 8 6 6" />
                 <path d="m4 14 6-6 2-3" />
                 <path d="M2 5h12" />
@@ -16,10 +16,13 @@
             </svg>
             @endif
         </div>
-        <select wire:model.live="lang" name="" id="" class=" select select-xs select-bordered focus-within:outline-none focus-within:border-info focus-within:ring-0 join-item">
+
+        <select
+            wire:model.live="lang"
+            class="rounded-l-none select select-bordered select-sm focus:outline-none focus:border-info join-item">
             <option value="">{{ __('Pilih Bahasa') }}</option>
-            <option value="en"><i class="fi fi-gb"></i>{{ __('English') }}</option>
-            <option value="id"><i class="fi fi-id"></i>{{ __('Bahasa Indonesia') }}</option>
+            <option value="en">English</option>
+            <option value="id">Bahasa Indonesia</option>
         </select>
     </div>
 </div>
