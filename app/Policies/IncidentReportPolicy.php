a3857ea8b5072fb4e5d8210439cf8948d347a14c
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-24 16:25:13 +0800

    sdfsdf

--- app/Policies/IncidentReportPolicy.php
+++ app/Policies/IncidentReportPolicy.php
@@ -38,13 +38,46 @@ class IncidentReportPolicy
     public function update(User $user, IncidentReport $incidentReport): bool
     {
         // 1. Admin HSE selalu boleh
-        if ($user->hasRole('admin-hse')) return true;
+        if ($user->hasRole('Administrator')) return true;
 
-        // 2. Pembuat laporan boleh (HANYA jika status masih 'Draft' atau 'Open')
-        if ($user->id === $incidentReport->created_by && in_array($incidentReport->status, ['draft', 'open'])) {
+        // 2. Jika laporan masih Open, Pembuat (Originator) bisa edit Part 1-2
+        if ($incidentReport->status === 'Open' && $user->id === $incidentReport->pelapor_id) {
+            return true;
+        }
+
+        // 3. Tim Investigasi yang terdaftar di Part 3
+        // Cek apakah user ID ada dalam relasi investigationTeams
+        if ($incidentReport->investigationTeams()->where('user_id', $user->id)->exists()) {
             return true;
         }
         return false;
+    }
+    public function close(User $user, IncidentReport $incident)
+    {
+        // Hanya Manager atau Superintendent yang bisa menutup laporan di Part 9
+        $isAssignedModerator = $user->moderatorAssignments()
+            ->where('event_type_id', $incident->event_type_id)
+            ->where(function (Builder $query) use ($incident) {
+
+                // Kriteria A: Penugasan bersifat umum (tidak spesifik pada Department/Contractor)
+                $query->whereNull('department_id')
+                    ->whereNull('contractor_id');
+
+                // Kriteria B: Penugasan spesifik untuk Department
+                if ($incident->department_id) {
+                    $query->orWhere('department_id', $incident->department_id);
+                }
+
+                // Kriteria C: Penugasan spesifik untuk Contractor
+                if ($incident->contractor_id) {
+                    $query->orWhere('contractor_id', $incident->contractor_id);
+                }
+            })
+            ->exists();
+
+        if ($isAssignedModerator) {
+            return true;
+        }
     }
 
     /**
