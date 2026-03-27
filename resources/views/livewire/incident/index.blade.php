5452f033c63696dc184d5ccb572518a7f8c72fde
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-27 13:57:53 +0800

    sas

--- resources/views/livewire/incident/index.blade.php
+++ resources/views/livewire/incident/index.blade.php
@@ -59,7 +59,7 @@
                             @php
                             $riskColor = match($item->risk?->rating_name) {
                             'Ekstrem' => 'bg-error text-error-content',
-                            'Tinggi' => 'bg-orange-500 text-white',
+                            'Tinggi' => 'bg-semi text-semi-content',
                             'Sedang' => 'bg-warning text-warning-content',
                             default => 'bg-success text-success-content',
                             };
@@ -74,7 +74,7 @@
                             <div @class([ 'badge badge-xs py-2 px-2 font-bold text-[9px] uppercase' , 'badge-success'=> $item->status === 'closed',
                                 'badge-error' => $item->status === 'Open',
                                 'badge-warning' => $item->status === 'Action Required',
-                                'badge-info' => $item->status === 'In Progress',
+                                'badge-semi' => $item->status === 'In Progress',
                                 ])>
                                 {{ $item->status }}
                             </div>
