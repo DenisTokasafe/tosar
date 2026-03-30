14fb08c08945c771d3f1341cf6f37bb124d5f430
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-30 10:57:33 +0800

    sfd

--- app/Livewire/Incident/Update.php
+++ app/Livewire/Incident/Update.php
@@ -1618,7 +1618,7 @@ class Update extends Component
             'step2' => $this->incident->involvedPersons()->exists(),
             'step3' => $this->incident->investigationTeams()->exists(),
             'step4' => $this->incident->peepoAnalyses()->whereNotNull('temuan')->where('temuan', '!=', '')->exists(),
-            'step5' => $this->incident->timelines()->whereNotNull('why_count_used')->exists(),
+            'step5' => $this->incident->timelines()->where('why_count_used', '!=', 0)->exists(),
             'step6' => !empty($this->incident->scat_analysis),
             'step7' => $this->incident->correctiveActions()->exists(),
 
