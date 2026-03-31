a5cb8cacdaa956ecca1faa151c07fecd3ae36e7e
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-03-31 09:52:07 +0800

    asdsad

--- resources/views/livewire/incident/index.blade.php
+++ resources/views/livewire/incident/index.blade.php
@@ -12,17 +12,24 @@
                 <thead>
                     <tr class="border-b bg-base-200 text-base-content border-base-300">
                         <th class="w-10 text-center">No</th>
-                        <th class="w-40">
-                            {{ __('Nomor Referensi') }}
-                            <!-- change popover-1 and --anchor-1 names. Use unique names for each dropdown -->
-                            <button class="btn btn-ghost btn-xs" popovertarget="popover-1" style="anchor-name:--anchor-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" class="{{ !empty($search) ? 'text-blue-600' : '' }}">
-                                    <circle cx="11" cy="11" r="8" />
-                                    <path d="m21 21-4.3-4.3" />
-                                </svg>
-                            </button>
-                            <div class="shadow-sm dropdown menu w-52 rounded-box bg-base-100" popover id="popover-1" style="position-anchor:--anchor-1">
-                                <x-form.input-text type="text" wire:model.live.debounce.300ms="search" placeholder="Cari nomor..." class="w-full input-sm" />
+                        <th class="w-40 text-center">
+                            <div class="flex items-center justify-center gap-1">
+                                {{ __('Nomor Referensi') }}
+
+                                {{-- Tombol Anchor --}}
+                                <button class="btn btn-ghost btn-xs" popovertarget="popover-1" style="anchor-name:--anchor-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" class="{{ !empty($search) ? 'text-blue-600' : '' }}">
+                                        <circle cx="11" cy="11" r="8" />
+                                        <path d="m21 21-4.3-4.3" />
+                                    </svg>
+                                </button>
+
+                                {{-- Popover Content --}}
+                                <div popover id="popover-1"
+                                    class="p-3 border shadow-xl rounded-box bg-base-100 w-60 border-base-300"
+                                    style="position-anchor:--anchor-1; inset-area: bottom; margin-top: 8px;">
+                                    <x-form.input-text type="text" wire:model.live.debounce.300ms="search" placeholder="Cari nomor..." class="w-full input-sm" />
+                                </div>
                             </div>
                         </th>
                         <th class="w-48">{{ __('Tanggal & Waktu') }}</th>
