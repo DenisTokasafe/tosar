6fae400487f1336340e88535182b3c7c0e009001
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-01 10:06:18 +0800

    xgdfgfd

--- app/Livewire/Incident/Update.php
+++ app/Livewire/Incident/Update.php
@@ -1540,7 +1540,6 @@ class Update extends Component
         $name = $this->manualName[$index][$type] ?? '';
 
         if (empty($name)) return;
-        dd($name);
 
         // 1. Set data ke array utama (sesuai struktur selectUser Anda)
         $this->{$type}[$index]['user_id'] = null; // null karena manual (tidak ada di DB)
