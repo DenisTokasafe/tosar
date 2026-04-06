3e325a2cabbb039890ac1ec31abd9626a86aa9a9
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-06 10:56:53 +0800

    asdf

--- resources/views/livewire/administration/translation/index.blade.php
+++ resources/views/livewire/administration/translation/index.blade.php
@@ -1,6 +1,7 @@
 <section class="w-full">
     @include('partials.translation')
-    <div class="flex items-center justify-end pb-2 border-b">
+    <div class="flex items-center justify-end pb-2 border-b w-full md:max-w-xs">
+        <x-form.input-text label="Cari Kata" model="search" required class="max-w-xs" />
         <x-form.input-text label="Cari Kata" model="search" required />
     </div>
     <x-manhours.layout>
