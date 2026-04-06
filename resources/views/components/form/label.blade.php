664746f71c62f8338b3a4cb06dc413af49bae4cc
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-06 11:55:43 +0800

    dsdfsdf

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,6 +1,20 @@
-<label {{ $attributes->merge(['class' => 'text-xs font-medium capitalize text-base-content ']) }}>
+@props([
+'label',
+'deskripsi' => false,
+'deskripsi_value' => ''
+])
+
+<label {{ $attributes->merge(['class' => 'text-xs font-medium capitalize text-base-content']) }}>
     {{ __($label) }}
+
     @if ($required)
-    <span class="font-bold text-red-500">*</span>
+    <span class="font-bold text-red-500 ml-0.5">
+        @if ($deskripsi)
+        <span class="text-[10px] font-normal italic mr-0.5">
+            {{ __($deskripsi_value) }}
+        </span>
+        @endif
+        <sup class="text-[10px]">*</sup>
+    </span>
     @endif
 </label>
