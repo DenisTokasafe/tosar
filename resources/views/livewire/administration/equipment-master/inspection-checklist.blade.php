d9390e12181c9d60d4288dae2206033cf518d830
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-07 09:00:26 +0800

    asdasd

--- resources/views/livewire/administration/equipment-master/inspection-checklist.blade.php
+++ resources/views/livewire/administration/equipment-master/inspection-checklist.blade.php
@@ -85,7 +85,7 @@
                                         placeholder="Contoh: Tekanan Oli" />
 
                                     <select wire:model="inputs.{{ $index }}.type" class="select select-bordered select-xs">
-                                        <option value="null">Label</option>
+                                        <option value="label">Label</option>
                                         <option value="text">Text</option>
                                         <option value="number">Number</option>
                                         <option value="date">Date</option>
