a7ff40fa5934f59cda9e1cd63b2fd6c9e6a4596f
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-08 08:56:50 +0800

    sdfsdf

--- resources/views/livewire/administration/equipment-master/inspection-checklist.blade.php
+++ resources/views/livewire/administration/equipment-master/inspection-checklist.blade.php
@@ -122,8 +122,8 @@
                         </div>
 
                         <div class="w-full form-control">
-                            <x-form.input-floating label="Location Keyword" type='text' model="location_keyword" placeholder="Location Keyword" />
-                            <!-- <x-form.search-floating label="Location Keyword" required modelsearch="searchLocation" modelid="location_id" placeholder="Location Keyword..." :options="$locations":showdropdown="$show_location" clickaction="selectLocation" namedb="name" /> -->
+                            <!-- <x-form.input-floating label="Location Keyword" type='text' model="location_keyword" placeholder="Location Keyword" /> -->
+                            <x-form.search-floating label="Location Keyword" required modelsearch="searchLocation" modelid="location_id" placeholder="Location Keyword..." :options="$locations" :showdropdown="$show_location" clickaction="selectLocation" namedb="name" />
 
                         </div>
 
