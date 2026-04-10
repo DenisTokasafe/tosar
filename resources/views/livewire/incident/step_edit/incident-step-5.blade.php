de9960bebd2f55e0157fa920d996a4f812a5d18f
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-10 20:50:04 +0800

    asdas

--- resources/views/livewire/incident/step_edit/incident-step-5.blade.php
+++ resources/views/livewire/incident/step_edit/incident-step-5.blade.php
@@ -4,12 +4,7 @@
         {{-- Kontrol di sisi kanan tanpa judul --}}
         <div class="flex gap-2">
             @if($canEdit)
-            <button wire:click="addWhyColumn" class="btn btn-primary btn-xs sm:btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                </svg>
-                <span class="hidden sm:inline">Tambah Why</span>
-            </button>
+
             <flux:tooltip content="{{ __('Klik untuk menambah tahapan analisis berikutnya') }}" position="top">
                 <flux:button icon="add-icon" wire:click="addWhyColumn" color="primary" size="xs">
                     <span class="hidden sm:inline">{{ __('Tambah Why') }}</span>
