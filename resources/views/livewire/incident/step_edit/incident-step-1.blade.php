a78fbe814eec8e2a7e397142c48ef4952c4ace64
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-13 13:08:02 +0800

    dsds

--- resources/views/livewire/incident/step_edit/incident-step-1.blade.php
+++ resources/views/livewire/incident/step_edit/incident-step-1.blade.php
@@ -236,7 +236,7 @@
             </label>
 
             <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
-                @foreach(\App\Models\BodyPart::whereIn('id', $selectedBodyParts)->get() as $part)
+                @foreach($this->detailsBodyPart as $part)
                 <label wire:key="body-part-{{ $part->id }}"
                     @class([ 'flex items-center gap-3 p-3 transition-all border rounded-md cursor-pointer group' , 'bg-primary/5 border-primary/30'=> in_array($part->id, $selectedBodyParts),
                     'bg-base-100 border-base-300 hover:bg-base-200' => !in_array($part->id, $selectedBodyParts),
