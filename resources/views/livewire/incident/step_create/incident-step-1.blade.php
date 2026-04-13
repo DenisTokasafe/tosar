2840c6dada6f4456832862a7ed73fe22ac64592e
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-13 10:27:40 +0800

    sa

--- resources/views/livewire/incident/step_create/incident-step-1.blade.php
+++ resources/views/livewire/incident/step_create/incident-step-1.blade.php
@@ -245,7 +245,7 @@
                 <label class="flex items-center gap-3 p-3 transition-all border rounded-md cursor-pointer hover:bg-white bg-base-100 border-base-300 group">
                     <input type="checkbox"
                         value="{{ $part->id }}"
-                        wire:model.live="selectedBodyParts"
+                        wire:model.live="selectedBodyPart"
                         class="checkbox checkbox-primary checkbox-sm">
                     <span class="text-sm group-hover:font-medium">{{ $part->name }}</span>
                 </label>
