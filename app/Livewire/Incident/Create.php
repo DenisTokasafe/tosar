b2bf5a895d0a0bd8caa72e1b740ba6728d09d300
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-04-22 08:45:06 +0800

    sdasd

--- app/Livewire/Incident/Create.php
+++ app/Livewire/Incident/Create.php
@@ -1272,7 +1272,7 @@ class Create extends Component
                 'destination' => '/incident/show/' . $result->id,
                 'backgroundColor' => "background: linear-gradient(135deg, #00c853, #00bfa5);",
             ]);
-            return $this->redirect(route('hazard-detail',  $this->link), navigate: true);
+            return $this->redirect(route('hazard-detail',  $this->link))a;
         } catch (\Illuminate\Validation\ValidationException $e) {
             $this->dispatchValidationEvents($e->validator->errors());
             $firstErrorField = collect($e->validator->errors()->keys())->first();
