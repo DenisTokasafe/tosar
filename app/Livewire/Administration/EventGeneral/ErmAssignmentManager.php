c7591beb652d5adc43d7ec1169737f30de6425ba
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-05-04 11:34:33 +0800

    asd

--- app/Livewire/Administration/EventGeneral/ErmAssignmentManager.php
+++ app/Livewire/Administration/EventGeneral/ErmAssignmentManager.php
@@ -67,6 +67,7 @@ class ErmAssignmentManager extends Component
         $this->department_id = $item->department_id;
         $this->contractor_id = $item->contractor_id;
         $this->status =  !empty($this->contractor_id) ? 'company'  : 'department';
+        dd($this->department_id);
 
         $this->dispatch('open-update-modal');
     }
