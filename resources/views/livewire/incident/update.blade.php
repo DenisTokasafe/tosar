b8366930e7fc41740ce8f560a8139ea782043415
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-05-12 14:44:26 +0800

    sd

--- resources/views/livewire/incident/update.blade.php
+++ resources/views/livewire/incident/update.blade.php
@@ -6,7 +6,7 @@
         {{ Breadcrumbs::render('incident-detail', $incidentId) }}
     </div>
 
-    <div class="space-y-6">
+    <div class="space-y-2">
         {{-- HEADER SECTION --}}
         <div class="w-full bg-base-200 rounded-full h-1.5 mb-2 overflow-hidden shadow-inner">
             <div class="h-full transition-all duration-700 ease-in-out bg-primary" style="width: {{ $this->getProgressPercentage() }}%"></div>
