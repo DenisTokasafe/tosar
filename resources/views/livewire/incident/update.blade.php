bb6e14ad92cd745759e1f10f675f572a9de3be89
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-05-13 08:11:20 +0800

    assd

--- resources/views/livewire/incident/update.blade.php
+++ resources/views/livewire/incident/update.blade.php
@@ -696,12 +696,12 @@
             </div>
 
             <div class="p-4 modal-action bg-gray-50">
-                <form method="dialog">
-                    <flux:button size="xs" variant="danger">Tutup</flux:button>
-                    <flux:button size="xs" variant="success" wire:click="sendAlert">
-                        Kirim Email
-                    </flux:button>
-                </form>
+
+                <flux:button size="xs" variant="danger" onclick="incident_modal.close()">Tutup</flux:button>
+                <flux:button size="xs" variant="success" wire:click="sendAlert">
+                    Kirim Email
+                </flux:button>
+
             </div>
             @endif
         </div>
