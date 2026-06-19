75898b8b8189855e1dca6a886f75fc0356d202da
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-06-19 08:25:36 +0800

    adssd

--- app/Models/User.php
+++ app/Models/User.php
@@ -140,6 +140,6 @@ class User extends Authenticatable implements LdapAuthenticatable
     public function hasAnyRole(array $roles)
     {
         // Menggunakan eager loading collection agar hemat query
-        return $this->roles->whereIn('name', $roles)->isNotEmpty();
+        return in_array($this->role?->name, $roles);
     }
 }
