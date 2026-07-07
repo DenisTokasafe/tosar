30d0b4598c0c0ba17bf1e566fab8282d48188057
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-07-07 15:12:48 +0800

    asdasdasdasdas

--- resources/views/components/tabs-event/layout.blade.php
+++ resources/views/components/tabs-event/layout.blade.php
@@ -1,5 +1,6 @@
+@if (auth()->User()->roles->pluck('name')->contains('administrator'))
 <div class="flex  flex-col ">
-     <div class=" overflow-x-auto w-auto md:overflow-x-hidden ">
+    <div class=" overflow-x-auto w-auto md:overflow-x-hidden ">
         <flux:navlist-horizontal>
             <flux:navlist-horizontal.item :href="route('administration-event_general-eventCategory')" wire:navigate>{{ __('Event Category') }}</flux:navlist-horizontal.item>
             <flux:navlist-horizontal.item :href="route('administration-event_general-eventType')" wire:navigate>{{ __('Event Type') }}</flux:navlist-horizontal.item>
@@ -8,6 +9,8 @@
             <flux:navlist-horizontal.item :href="route('administration-event_general-ErmAssignmentManager')" wire:navigate>{{ __('ERM Assigment') }}</flux:navlist-horizontal.item>
         </flux:navlist-horizontal>
     </div>
+
+
 
 
     {{-- <flux:separator class="md:hidden" /> --}}
@@ -20,3 +23,18 @@
         </div>
     </div>
 </div>
+@else
+<div class="z-30 flex items-start max-md:flex-col ">
+    <div class="self-stretch flex-1 max-md:pt-6">
+        <flux:heading>{{ $heading ?? '' }}</flux:heading>
+        <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
+        <div class="flex w-full flex-1 flex-col gap-4 rounded-xl inset-shadow-sm h-full max-h-[calc(100vh-16rem)] sm:max-h-[calc(100vh-16rem)] md:max-h-[calc(100vh-14rem)] lg:max-h-[calc(100vh-14rem)] 2xl:max-h-[calc(100vh-14rem)] relative">
+            <div class="flex-1 h-full p-4 overflow-x-hidden overflow-y-auto border rounded-xl border-neutral-200 dark:border-base-200">
+                <div class="w-full max-w-full ">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
