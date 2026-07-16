cdce7a9b8b7266e8137bc3d870c8b0ef5409a8f8
yomangit <212838158+yoms-den@users.noreply.github.com>
2026-07-16 10:25:16 +0800

    asas

--- resources/views/livewire/people/add-people.blade.php
+++ resources/views/livewire/people/add-people.blade.php
@@ -1,7 +1,7 @@
 <div>
     <!-- Open the modal using ID.showModal() method -->
     <button class="btn" onclick="my_modal_1.showModal()">open modal</button>
-    <dialog id="my_modal_1" class="modal">
+    <dialog id="my_modal_1" wire.ignore.self class="modal">
         <div class="modal-box">
             <h3 class="font-bold text-lg">Hello!</h3>
             <form wire:submit="register">
